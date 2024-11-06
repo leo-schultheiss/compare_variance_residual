ec8b1e6eda5cbecb939eb0d7b81e80275af46e19
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-06 11:40:14 +0100

    simplify visualizing subject correlation data

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -5,8 +5,6 @@ import numpy as np
 
 from common_utils.hdf_utils import map_to_flat
 
-# Get path to data files
-fdir = os.path.abspath('../../data')
 
 modality = "reading"
 subject = "01"
@@ -14,12 +12,18 @@ layer = 11
 
 correlation_data = np.load(os.path.join("../../bert-predictions", modality, subject, f"layer_{layer}.npy"), allow_pickle=True)
 
-# Map to subject flatmap
-map_file = os.path.join(fdir, 'subject{}_mappers.hdf'.format(subject))
-flatmap = map_to_flat(correlation_data, map_file)
 
-# Plot flatmap
-fig, ax = plt.subplots()
-_ = ax.imshow(flatmap, cmap='inferno')
-ax.axis('off')
-plt.show()
+def plot_subject_voxel_data(subject_number, data):
+    # Get path to data files
+    fdir = os.path.abspath('../../data')
+    # Map to subject flatmap
+    map_file = os.path.join(fdir, f'subject{subject_number}_mappers.hdf')
+    flatmap = map_to_flat(data, map_file)
+    # Plot flatmap
+    fig, ax = plt.subplots()
+    _ = ax.imshow(flatmap, cmap='inferno')
+    ax.axis('off')
+    plt.show()
+
+
+plot_subject_voxel_data(subject, correlation_data)
