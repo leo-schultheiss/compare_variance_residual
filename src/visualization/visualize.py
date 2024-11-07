fc134f8230aef1b60f032b25289e3984a0fb17e9
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-07 19:46:24 +0100

    visualize all layers

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from common_utils.hdf_utils import map_to_flat
 
-def plot_subject_voxel_data(subject_number, data):
+def flatmap_subject_voxel_data(subject_number, data):
     # Get path to data files
     fdir = os.path.abspath('../../data')
     # Map to subject flatmap
@@ -16,23 +16,27 @@ def plot_subject_voxel_data(subject_number, data):
     _ = ax.imshow(flatmap, cmap='inferno')
     ax.axis('off')
     plt.show()
+    return fig
 
 
 modality = "reading"
 subject = "01"
-layer = 11
-
-# 1D arrays of correlation data
-correlation_data =  [
-    np.load(os.path.join("../../bert-predictions", modality, subject, f"layer_{layer}.npy"), allow_pickle=True),
-    np.load(os.path.join("../../bert-joint-predictions", subject, modality, "letters",f"joint_model_prediction_voxelwise_correlation_layer{layer}.npy"), allow_pickle=True),
-]
-
-# calculate average difference between the two
-diff = np.nan_to_num(correlation_data[0]) - np.nan_to_num(correlation_data[1])
-print(f"Average difference between the two models: {np.mean(diff)}")
-print(f"maximum difference between the two models: {np.max(diff)}")
 
 
-for data in correlation_data:
-    plot_subject_voxel_data(subject, data)
+for layer in range(12):
+    joint_correlation = np.load(os.path.join("../../bert-predictions", modality, subject, f"layer_{layer}.npy"), allow_pickle=True)
+    standard_correlation = np.load(os.path.join("../../bert-joint-predictions", subject, modality, "letters", f"joint_model_prediction_voxelwise_correlation_layer{layer}.npy"), allow_pickle=True)
+    residual_correlation = np.load(os.path.join("../../bert-residuals", modality, "numwords", subject, f"layer_{layer}.npy"), allow_pickle=True)
+
+    # calculate average difference between the two
+    diff = np.nan_to_num(standard_correlation) - np.nan_to_num(joint_correlation)
+    print(f"Average difference between the two models: {np.mean(diff)}")
+    print(f"maximum difference between the two models: {np.max(diff)}")
+    print(f"minimum difference between the two models: {np.min(np.abs(diff))}")
+    print(f"number of voxels with difference equal to 0.0: {np.sum(diff == 0.0)}")
+    print(f"mean of the first model: {np.mean(np.nan_to_num(standard_correlation))}")
+    print(f"mean of the second model: {np.mean(np.nan_to_num(joint_correlation))}")
+
+    fig = flatmap_subject_voxel_data(subject, joint_correlation)
+    os.makedirs(f"../../plots/{modality}/{subject}", exist_ok=True)
+    fig.savefig(f"../../plots/{modality}/{subject}/joint_correlation_layer_{layer}.png")
