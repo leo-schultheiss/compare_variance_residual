0f69dab56dca6247dbdd195335ebb4d2995bd6ad
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-07 10:10:16 +0100

    add code for comparing model performances

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -5,14 +5,6 @@ import numpy as np
 
 from common_utils.hdf_utils import map_to_flat
 
-
-modality = "reading"
-subject = "01"
-layer = 11
-
-correlation_data = np.load(os.path.join("../../bert-predictions", modality, subject, f"layer_{layer}.npy"), allow_pickle=True)
-
-
 def plot_subject_voxel_data(subject_number, data):
     # Get path to data files
     fdir = os.path.abspath('../../data')
@@ -26,4 +18,21 @@ def plot_subject_voxel_data(subject_number, data):
     plt.show()
 
 
-plot_subject_voxel_data(subject, correlation_data)
+modality = "reading"
+subject = "01"
+layer = 11
+
+# 1D arrays of correlation data
+correlation_data =  [
+    np.load(os.path.join("../../bert-predictions", modality, subject, f"layer_{layer}.npy"), allow_pickle=True),
+    np.load(os.path.join("../../bert-joint-predictions", subject, modality, "letters",f"joint_model_prediction_voxelwise_correlation_layer{layer}.npy"), allow_pickle=True),
+]
+
+# calculate average difference between the two
+diff = np.nan_to_num(correlation_data[0]) - np.nan_to_num(correlation_data[1])
+print(f"Average difference between the two models: {np.mean(diff)}")
+print(f"maximum difference between the two models: {np.max(diff)}")
+
+
+for data in correlation_data:
+    plot_subject_voxel_data(subject, data)
