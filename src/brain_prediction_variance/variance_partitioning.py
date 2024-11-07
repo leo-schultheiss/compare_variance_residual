b435c335ee67af5e23e7789e8aeba78a8a74c6f7
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-07 08:52:18 +0100

    implement first version of joint model training

--- src/brain_prediction_variance/variance_partitioning.py
+++ src/brain_prediction_variance/variance_partitioning.py
@@ -5,14 +5,14 @@ import h5py
 import numpy as np
 from ridge_utils.dsutils import make_word_ds, make_phoneme_ds, make_semantic_model
 from ridge_utils.ridge import bootstrap_ridge
-from torch.ao.quantization import default_embedding_fake_quant
 
-from brain_prediction_standard.predict_brain_activity import data_dir
 from common_utils.SemanticModel import SemanticModel
 from common_utils.hdf_utils import load_subject_fmri
 from common_utils.npp import zscore
 from common_utils.stimulus_utils import load_grids_for_stories, load_generic_trfiles
 from common_utils.util import make_delayed
+
+data_dir = "data"
 
 
 def signed_squared_correlation(rho):
@@ -36,51 +36,59 @@ def intersection(rho_1, rho_2, rho_1_union_rho_2):
     return math.sqrt(signed_squared_correlation(rho_1) + signed_squared_correlation(rho_2) - signed_squared_correlation(
         rho_1_union_rho_2))
 
+
 def load_low_level_textual_features():
     """
     These files contain low-level textual and speech features
     """
     # 'letters', 'numletters', 'numphonemes', 'numwords', 'phonemes', 'word_length_std'
-    base_features_train = h5py.File(os.path.join(data_dir, 'features_trn_NEW.hdf'), 'r+')
-    base_features_val = h5py.File(os.path.join(data_dir,'features_val_NEW.hdf'), 'r+')
+    base_features_train = h5py.File(os.path.join(data_dir, 'features_trn_NEW.hdf'), 'r')
+    base_features_val = h5py.File(os.path.join(data_dir, 'features_val_NEW.hdf'), 'r')
     return base_features_train, base_features_val
 
 
-def train_joint_model(Rstim, Pstim, data_dir, subject, modality, num_layers=12):
+def prediction_joint_model(Rstim_1, Rstim_2, Pstim_1, Pstim_2, data_dir, subject, modality, num_layers=12):
     """
     Train a joint model for two feature spaces
-    :param Rstim – Training stimuli with TR time points and N features. Each feature should be Z-scored across time.
-    :param Pstim – Test stimuli with TP time points and N features. Each feature should be Z-scored across time.
-    :param data_dir – Directory containing fMRI data
-    :param subject – Subject number
-    :param modality – Modality of the data
-
+    :param Rstim_1 Training stimuli with TR time points and N features. Each feature should be Z-scored across time
+    :param Rstim_1 Training stimuli with TR time points and M features. Each feature should be Z-scored across time
+    :param Pstim_1 Test stimuli with TP time points and O features. Each feature should be Z-scored across time
+    :param Pstim_1 Test stimuli with TP time points and P features. Each feature should be Z-scored across time
+    :param data_dir Directory containing fMRI data
+    :param subject Subject number from 1 to 9
+    :param modality Modality of the data, reading or listening
     :return joint_model_predictions – Predictions of the joint model per layer
     """
     # Run regression
     nboots = 1  # Number of cross-validation runs.
-    chunklen = 40  #
-    nchunks = 20
+    chunklen = 40 # Length of chunks to break data into.
+    nchunks = 20 # Number of chunks to use in the cross-validated training.
     correlations_per_layer = []
+    pred = []
     for layer_number in np.arange(num_layers):
         # Training responses with TR time points and M different responses
         zRresp, zPresp = load_subject_fmri(data_dir, subject, modality)
-        alphas = np.logspace(1, 3,
-                             10)  # Equally log-spaced alphas between 10 and 1000. The third number is the number of alphas to test.
-        wt, corr, alphas, bscorrs, valinds = bootstrap_ridge(np.nan_to_num(Rstim[layer_number]), zRresp,
-                                                             np.nan_to_num(Pstim[layer_number]), zPresp,
+        # Equally log-spaced alphas between 10 and 1000. The third number is the number of alphas to test.
+        alphas = np.logspace(1, 3, 10)
+        # join input features (context representations and low-level textual features)
+        Rstim = np.hstack((Rstim_1[layer_number], Rstim_2))
+        Pstim = np.hstack([Pstim_1[layer_number], Pstim_2])
+        print("Rstim shape: ", Rstim.shape)
+        print("Pstim shape: ", Pstim.shape)
+        wt, corr, alphas, bscorrs, valinds = bootstrap_ridge(np.nan_to_num(Rstim), zRresp,
+                                                             np.nan_to_num(Pstim), zPresp,
                                                              alphas, nboots, chunklen, nchunks,
                                                              singcutoff=1e-10, single_alpha=True)
-        pred = np.dot(np.nan_to_num(delayed_Pstim[layer_number]), wt)
+        pred = np.dot(np.nan_to_num(Pstim), wt)
+        print(pred.shape)
+        print(zPresp.shape)
+        voxelwise_correlations = np.zeros((zPresp.shape[1],))  # create zero-filled array to hold correlations
+        for voxel_index in range(zPresp.shape[1]):
+            voxelwise_correlations[voxel_index] = np.corrcoef(zPresp[:, voxel_index], pred[:, voxel_index])[0, 1]
+        print(voxelwise_correlations)
+        correlations_per_layer.append(voxelwise_correlations)
+    return correlations_per_layer, pred
 
-        # np.save(os.path.join(main_dir+'/'+save_dir, "test_"+str(eachlayer)),zPresp)
-        # np.save(os.path.join(main_dir+'/'+save_dir, "pred_"+str(eachlayer)),pred)
-        voxcorrs = np.zeros((zPresp.shape[1],))  # create zero-filled array to hold correlations
-        for vi in range(zPresp.shape[1]):
-            voxcorrs[vi] = np.corrcoef(zPresp[:, vi], pred[:, vi])[0, 1]
-        print(voxcorrs)
-        correlations_per_layer.append(voxcorrs)
-    return correlations_per_layer
 
 if __name__ == '__main__':
     import argparse
@@ -92,11 +100,13 @@ if __name__ == '__main__':
     parser.add_argument("-s", "--subjectNum", help="Subject number", type=int, required=True)
     parser.add_argument("--modality", help="Choose modality", type=str, default="reading")
     parser.add_argument("--layers", help="Number of layers", type=int, required=True)
-    parser.add_argument('--model_1', type=str, help='numpy file containing model prediction correlation data')
-    parser.add_argument('--model_2', type=str, help='numpy file containing model prediction correlation data')
+    parser.add_argument("--low_level_feature",
+                        help="Low level feature to use. Possible options include:\n"
+                             "letters, numletters, numphonemes, numwords, phonemes, word_length_std",
+                        type=str, default="letters")
+    parser.add_argument("output_dir", help="Output directory", type=str)
     args = parser.parse_args()
 
-    subject = f'0{args.subjectNum}'
     context_representations = np.load(args.context_representations, allow_pickle=True)
     # print(context_representations.item().keys())
 
@@ -112,8 +122,7 @@ if __name__ == '__main__':
     trfiles = load_generic_trfiles(all_story_names, root="stimuli/trfiles")
 
     # Make word and phoneme datasequences
-    wordseqs = make_word_ds(grids, trfiles)  # dictionary of {storyname : word DataSequence}
-    phonseqs = make_phoneme_ds(grids, trfiles)  # dictionary of {storyname : phoneme DataSequence}
+    word_data_sequences = make_word_ds(grids, trfiles)  # dictionary of {storyname : word DataSequence}
     eng1000 = SemanticModel.load(os.path.join(args.data_dir, "english1000sm.hf5"))
 
     semantic_sequence_representations = dict()  # dictionary to hold projected stimuli {story name : projected DataSequence}
@@ -121,7 +130,7 @@ if __name__ == '__main__':
         print(all_story_names[i])
         semantic_sequence_representations[all_story_names[i]] = []
         for layer in np.arange(args.layers):
-            temp = make_semantic_model(wordseqs[all_story_names[i]], [eng1000], [985])
+            temp = make_semantic_model(word_data_sequences[all_story_names[i]], [eng1000], [985])
             temp.data = np.nan_to_num(context_representations.item()[all_story_names[i]][layer])
             semantic_sequence_representations[all_story_names[i]].append(temp)
 
@@ -170,18 +179,39 @@ if __name__ == '__main__':
         delayed_Pstim.append(make_delayed(np.array(prediction_stim[layer])[0], delays))
 
     # Print the sizes of these matrices
-    print("delRstim shape: ", delayed_Rstim[0].shape)
-    print("delPstim shape: ", delayed_Pstim[0].shape)
+    print("delayed_Rstim shape: ", delayed_Rstim[0].shape)
+    print("delayed_Pstim shape: ", delayed_Pstim[0].shape)
 
-    joint_model_predictions = train_joint_model(delayed_Rstim, delayed_Pstim, args.data_dir, subject, args.modality, args.layers)
+    # join input features (context representations and low-level textual features)
+    base_features_train, base_features_val = load_low_level_textual_features()
+    print("base features train shape: ", np.shape(base_features_train))
+    print("base features val shape: ", np.shape(base_features_val))
 
-    main_dir = os.path.join(args.data_dir, args.modality, subject)
+    trim = 5
+    np.random.seed(9)
+
+    z_base_feature_train = np.vstack(
+        [zscore(base_features_train[story][args.low_level_feature][5 + trim:-trim]) for story in
+         base_features_train.keys()])
+    z_base_feature_val = np.vstack(
+        [zscore(base_features_val[story][args.low_level_feature][5 + trim:-trim]) for story in
+         base_features_val.keys()])
+
+    subject = f'0{args.subjectNum}'
+
+    voxelxise_correlations, predictions = prediction_joint_model(delayed_Rstim, z_base_feature_train,
+                                                                 delayed_Pstim, z_base_feature_val,
+                                                                 args.data_dir,
+                                                                 subject,
+                                                                 args.modality, args.layers)
+
+    # save voxelwise correlations and predictions
+    main_dir = os.path.join(args.output_dir, args.modality, subject, args.low_level_feature)
     if not os.path.exists(main_dir):
         os.makedirs(main_dir)
 
-    # if model_1 and model_2 args are given, load them
-    if args.model_1 and args.model_2:
-        model_1 = np.load(args.model_1, allow_pickle=True)
-        model_2 = np.load(args.model_2, allow_pickle=True)
-    else:
-        raise NotImplementedError("Must provide model_1 and model_2 args")
+    for layer in range(len(voxelxise_correlations)):
+        np.save(
+            os.path.join(str(main_dir), args.modality, f"joint_model_prediction_voxelwise_correlation_layer{layer}"),
+            voxelxise_correlations[layer])
+        # np.save(os.path.join(main_dir, f"predictions_{layer}"), predictions[layer])
