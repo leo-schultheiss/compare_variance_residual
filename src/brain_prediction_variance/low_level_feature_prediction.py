448f657dac8ef8a7db2cc3d1784fba7d1e88dc4f
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-11 15:02:10 +0100

    allow for 0 delays in low level features

--- src/brain_prediction_variance/low_level_feature_prediction.py
+++ src/brain_prediction_variance/low_level_feature_prediction.py
@@ -1,50 +1,8 @@
 import os.path
-import threading
 
-import h5py
 import numpy as np
-import logging
 
-from numpy import number
-from ridge_utils.dsutils import make_word_ds, make_phoneme_ds, make_semantic_model
-from ridge_utils.ridge import bootstrap_ridge
-
-from common_utils.SemanticModel import SemanticModel, logger
-from common_utils.hdf_utils import load_subject_fmri
-from common_utils.npp import zscore
-from common_utils.stimulus_utils import load_grids_for_stories, load_generic_trfiles
-from common_utils.util import make_delayed, create_delayed_low_level_feature
-
-logging.basicConfig(level=logging.DEBUG)
-
-
-def prediction_joint_model(Rstim, Pstim, data_dir, subject, modality):
-    """
-    Train a joint model for two feature spaces
-    :param Rstim Training stimuli with TR time points and N features. Each feature should be Z-scored across time
-    :param Pstim Test stimuli with TP time points and M features. Each feature should be Z-scored across time
-    :param data_dir Directory containing fMRI data
-    :param subject Subject number from 1 to 9
-    :param modality Modality of the data, reading or listening
-    :return joint_model_predictions – Predictions of the joint model per layer
-    """
-    # Run regression
-    nboots = 1  # Number of cross-validation runs.
-    chunklen = 40  # Length of chunks to break data into.
-    nchunks = 20  # Number of chunks to use in the cross-validated training.
-    # Training responses with TR time points and M different responses
-    zRresp, zPresp = load_subject_fmri(data_dir, subject, modality)
-    # Equally log-spaced alphas between 10 and 1000. The third number is the number of alphas to test.
-    alphas = np.logspace(1, 3, 10)
-    wt, corr, alphas, bscorrs, valinds = bootstrap_ridge(np.nan_to_num(Rstim), zRresp,
-                                                         np.nan_to_num(Pstim), zPresp,
-                                                         alphas, nboots, chunklen, nchunks,
-                                                         singcutoff=1e-10, single_alpha=True)
-    prediction = np.dot(np.nan_to_num(Pstim), wt)
-    voxelwise_correlations = np.zeros((zPresp.shape[1],))  # create zero-filled array to hold correlations
-    for voxel_index in range(zPresp.shape[1]):
-        voxelwise_correlations[voxel_index] = np.corrcoef(zPresp[:, voxel_index], prediction[:, voxel_index])[0, 1]
-    return voxelwise_correlations
+from common_utils.training_utils import create_delayed_low_level_feature, run_regression_and_predict
 
 
 def train_low_level_model(data_dir, subject_num, modality, low_level_feature, output_dir, numer_of_delays=6):
@@ -53,8 +11,8 @@ def train_low_level_model(data_dir, subject_num, modality, low_level_feature, ou
     z_base_feature_train, z_base_feature_val = create_delayed_low_level_feature(data_dir, delays, low_level_feature)
 
     subject = f'0{subject_num}'
-    voxelwise_correlations = prediction_joint_model(z_base_feature_train, z_base_feature_val, data_dir, subject,
-                                                    modality)
+    voxelwise_correlations = run_regression_and_predict(z_base_feature_train, z_base_feature_val, data_dir, subject,
+                                                        modality)
     # save voxelwise correlations and predictions
     main_dir = os.path.join(output_dir, modality, subject, low_level_feature)
     if not os.path.exists(main_dir):
@@ -70,21 +28,30 @@ if __name__ == '__main__':
     parser.add_argument("-d", "--data_dir", help="Directory containing data", type=str, default="data")
     parser.add_argument("-s", "--subject_num", help="Subject number", type=int, required=True)
     parser.add_argument("-m", "--modality", help="Choose modality", type=str, default="reading")
-    parser.add_argument("--low_level_feature",
-                        help="Low level feature to use. Possible options include:\n"
-                             "letters, numletters, numphonemes, numwords, phonemes, word_length_std",
+    parser.add_argument("--low_level_feature", help="Low level feature to use. Possible options include:\n"
+                                                    "letters, numletters, numphonemes, numwords, phonemes, word_length_std",
                         type=str, default="letters")
     parser.add_argument("output_dir", help="Output directory", type=str)
     args = parser.parse_args()
     print(args)
 
-    threads = []
-    for i in range(1, 8):
-        thread = threading.Thread(
-            train_low_level_model(args.data_dir, args.subject_num, args.modality, args.low_level_feature,
-                                  args.output_dir, i))
-        threads.append(thread)
-        thread.start()
+    # use processes instead of threads
+    processes = []
+    import multiprocessing
 
-    for thread in threads:
-        thread.join()
+    for i in range(1, 15):
+        p = multiprocessing.Process(target=train_low_level_model, args=(
+            args.data_dir, args.subject_num, args.modality, args.low_level_feature, args.output_dir, i))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    # compare results
+    for i in range(1, 15):
+        main_dir = os.path.join(args.output_dir, args.modality, f'0{args.subject_num}', args.low_level_feature)
+        voxelwise_correlations = np.load(
+            os.path.join(str(main_dir), f"low_level_model_prediction_voxelwise_correlation_delays{i}.npy"))
+        print(
+            f"Voxelwise correlation for {args.low_level_feature} with {i} delays: {np.nan_to_num(voxelwise_correlations).mean()}")
---
+++ src/common_utils/training_utils.py
@@ -0,0 +1,114 @@
+import os
+
+import h5py
+import numpy as np
+from ridge_utils.ridge import bootstrap_ridge
+
+from common_utils.hdf_utils import load_data
+from common_utils.npp import zscore
+
+
+def make_delayed(stim, delays, circpad=False):
+    """Creates non-interpolated concatenated delayed versions of [stim] with the given [delays]
+    (in samples).
+
+    If [circpad], instead of being padded with zeros, [stim] will be circularly shifted.
+    """
+    nt, ndim = stim.shape
+    dstims = []
+    for di, d in enumerate(delays):
+        dstim = np.zeros((nt, ndim))
+        if d < 0:  ## negative delay
+            dstim[:d, :] = stim[-d:, :]
+            if circpad:
+                dstim[d:, :] = stim[:-d, :]
+        elif d > 0:
+            dstim[d:, :] = stim[:-d, :]
+            if circpad:
+                dstim[:d, :] = stim[-d:, :]
+        else:  ## d==0
+            dstim = stim.copy()
+        dstims.append(dstim)
+    return np.hstack(dstims)
+
+
+def load_subject_fmri(data_dir, subject, modality):
+    """Load fMRI data for a subject, z-scored across stories"""
+    fname_tr5 = os.path.join(data_dir, 'subject{}_{}_fmri_data_trn.hdf'.format(subject, modality))
+    trndata5 = load_data(fname_tr5)
+
+    fname_te5 = os.path.join(data_dir, 'subject{}_{}_fmri_data_val.hdf'.format(subject, modality))
+    tstdata5 = load_data(fname_te5)
+
+    trim = 5
+    zRresp = np.vstack([zscore(trndata5[story][5 + trim:-trim - 5]) for story in trndata5.keys()])
+    zPresp = np.vstack([zscore(tstdata5[story][1][5 + trim:-trim - 5]) for story in tstdata5.keys()])
+
+    return zRresp, zPresp
+
+
+def load_low_level_textual_features(data_dir):
+    """
+    These files contain low-level textual and speech features
+    """
+    # 'letters', 'numletters', 'numphonemes', 'numwords', 'phonemes', 'word_length_std'
+    base_features_train = h5py.File(os.path.join(data_dir, 'features_trn_NEW.hdf'), 'r')
+    base_features_val = h5py.File(os.path.join(data_dir, 'features_val_NEW.hdf'), 'r')
+    return base_features_train, base_features_val
+
+
+def create_delayed_low_level_feature(data_dir, delays, low_level_feature):
+    base_features_train, base_features_val = load_low_level_textual_features(data_dir)
+    if delays > 0:
+        delayed_feature_train = []
+        for story in base_features_train.keys():
+            delayed = make_delayed(base_features_train[story][low_level_feature], delays)
+            delayed_feature_train.append(delayed)
+        delayed_feature_val = []
+        for story in base_features_val.keys():
+            delayed = make_delayed(base_features_val[story][low_level_feature], delays)
+            delayed_feature_val.append(delayed)
+        trim = 5
+        np.random.seed(9)
+        z_base_feature_train = np.vstack(
+            [zscore(delayed_feature_train[story][5 + trim:-trim]) for story in range(len(delayed_feature_train))])
+        z_base_feature_val = np.vstack(
+            [zscore(delayed_feature_val[story][5 + trim:-trim]) for story in range(len(delayed_feature_val))])
+        return z_base_feature_train, z_base_feature_val
+    else:
+        trim = 5
+        np.random.seed(9)
+        z_base_feature_train = np.vstack(
+            [zscore(base_features_train[story][5 + trim:-trim]) for story in base_features_train.keys()])
+        z_base_feature_val = np.vstack(
+            [zscore(base_features_val[story][5 + trim:-trim]) for story in base_features_val.keys()])
+        return z_base_feature_train, z_base_feature_val
+
+
+def run_regression_and_predict(Rstim, Pstim, data_dir, subject, modality):
+    """
+    Train a joint model for two feature spaces
+    :param Rstim Training stimuli with TR time points and N features. Each feature should be Z-scored across time
+    :param Pstim Test stimuli with TP time points and M features. Each feature should be Z-scored across time
+    :param data_dir Directory containing fMRI data
+    :param subject number from 1 to 9
+    :param modality Type of modality of the data, reading or listening
+    :return voxelwise_correlations – Predictions of the joint model per layer
+    """
+    # Run regression
+    nboots = 1  # Number of cross-validation runs.
+    chunklen = 40  # Length of chunks to break data into.
+    nchunks = 20  # Number of chunks to use in the cross-validated training.
+    # Training responses with TR time points and M different responses
+    zRresp, zPresp = load_subject_fmri(data_dir, subject, modality)
+    # Equally log-spaced alphas between 10 and 1000. The third number is the number of alphas to test.
+    alphas = np.logspace(1, 3, 10)
+    wt, corr, alphas, bscorrs, valinds = bootstrap_ridge(np.nan_to_num(Rstim), zRresp,
+                                                         np.nan_to_num(Pstim), zPresp,
+                                                         alphas, nboots, chunklen, nchunks,
+                                                         singcutoff=1e-10, single_alpha=True)
+    prediction = np.dot(np.nan_to_num(Pstim), wt)
+    voxelwise_correlations = np.zeros((zPresp.shape[1],))  # create zero-filled array to hold correlations
+    for voxel_index in range(zPresp.shape[1]):
+        voxelwise_correlations[voxel_index] = np.corrcoef(zPresp[:, voxel_index], prediction[:, voxel_index])[0, 1]
+    return voxelwise_correlations
