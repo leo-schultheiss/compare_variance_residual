9d80b97f0f8b685f3df64e28ead433f98c020b59
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-11 09:56:16 +0100

    move main logic out of main method

--- src/brain_prediction_variance/joint_prediction.py
+++ src/brain_prediction_variance/joint_prediction.py
@@ -81,6 +81,98 @@ def prediction_joint_model(Rstim, Pstim, data_dir, subject, modality, num_layers
     return correlations_per_layer
 
 
+def predict_joint_model(data_dir, context_representations, subject_num, modality, num_layers, low_level_feature,
+                        output_dir):
+    context_representations = np.load(context_representations, allow_pickle=True)
+    training_story_names = ['alternateithicatom', 'avatar', 'howtodraw', 'legacy',
+                            'life', 'myfirstdaywiththeyankees', 'naked',
+                            'odetostepfather', 'souls', 'undertheinfluence']
+    testing_story_names = ['wheretheressmoke']
+    all_story_names = training_story_names + testing_story_names
+    grids = load_grids_for_stories(all_story_names)
+    # Load TRfiles
+    trfiles = load_generic_trfiles(all_story_names, root="stimuli/trfiles")
+    # Make word and phoneme datasequences
+    word_data_sequences = make_word_ds(grids, trfiles)  # dictionary of {storyname : word DataSequence}
+    eng1000 = SemanticModel.load(os.path.join(data_dir, "english1000sm.hf5"))
+
+    semantic_sequence_representations = dict()  # dictionary to hold projected stimuli {story name : projected DataSequence}
+    for i in np.arange(len(all_story_names)):
+        print(all_story_names[i])
+        semantic_sequence_representations[all_story_names[i]] = []
+        for layer in np.arange(num_layers):
+            temp = make_semantic_model(word_data_sequences[all_story_names[i]], [eng1000], [985])
+            temp.data = np.nan_to_num(context_representations.item()[all_story_names[i]][layer])
+            semantic_sequence_representations[all_story_names[i]].append(temp)
+    # Downsample stimuli, since fMRI data covers multiple words in one TR
+    interpolation_type = "lanczos"  # filter type used for averaging across representations
+    window = 3  # number of lobes in Lanczos filter
+    # num_layers = 12
+    down_sampled_semantic_sequences = dict()  # dictionary to hold down-sampled stimuli
+    for story in all_story_names:
+        down_sampled_semantic_sequences[story] = []
+        for layer in np.arange(num_layers):
+            temp = semantic_sequence_representations[story][layer].chunksums(interpolation_type, window=window)
+            down_sampled_semantic_sequences[story].append(temp)
+
+    trim = 5
+    training_stim = {}
+    prediction_stim = {}
+    for layer in np.arange(num_layers):
+        training_stim[layer] = []
+        training_stim[layer].append(
+            np.vstack(
+                [zscore(down_sampled_semantic_sequences[story][layer][5 + trim:-trim]) for story in
+                 training_story_names]))
+    for layer in np.arange(num_layers):
+        prediction_stim[layer] = []
+        prediction_stim[layer].append(
+            np.vstack(
+                [zscore(down_sampled_semantic_sequences[story][layer][5 + trim:-trim]) for story in
+                 testing_story_names]))
+    story_lengths = [len(down_sampled_semantic_sequences[story][0][5 + trim:-trim]) for story in training_story_names]
+    print(story_lengths)
+    # Delay stimuli to account for hemodynamic lag
+    numer_of_delays = 6
+    delays = range(1, numer_of_delays + 1)
+    print("FIR model delays: ", delays)
+    print(np.array(training_stim[0]).shape)
+    delayed_Rstim = []
+    for layer in np.arange(num_layers):
+        delayed_Rstim.append(make_delayed(np.array(training_stim[layer])[0], delays))
+    delayed_Pstim = []
+    for layer in np.arange(num_layers):
+        delayed_Pstim.append(make_delayed(np.array(prediction_stim[layer])[0], delays))
+    # Print the sizes of these matrices
+    print("delayed_Rstim shape: ", delayed_Rstim[0].shape)
+    print("delayed_Pstim shape: ", delayed_Pstim[0].shape)
+    # join input features (context representations and low-level textual features)
+    base_features_train, base_features_val = load_low_level_textual_features()
+    trim = 5
+    np.random.seed(9)
+    z_base_feature_train = np.vstack(
+        [zscore(base_features_train[story][low_level_feature][5 + trim:-trim]) for story in
+         base_features_train.keys()])
+    z_base_feature_val = np.vstack(
+        [zscore(base_features_val[story][low_level_feature][5 + trim:-trim]) for story in
+         base_features_val.keys()])
+    print("base features train shape: ", np.shape(z_base_feature_train))
+    print("base features val shape: ", np.shape(z_base_feature_val))
+    # join input features (context representations and low-level textual features)
+    Rstim = [np.hstack((delayed_Rstim[layer_number], z_base_feature_train)) for layer_number in np.arange(num_layers)]
+    Pstim = [np.hstack([delayed_Pstim[layer_number], z_base_feature_val]) for layer_number in np.arange(num_layers)]
+    subject = f'0{subject_num}'
+    voxelxise_correlations = prediction_joint_model(Rstim, Pstim, data_dir, subject, modality,
+                                                    num_layers)
+    # save voxelwise correlations and predictions
+    main_dir = os.path.join(output_dir, modality, subject, low_level_feature)
+    if not os.path.exists(main_dir):
+        os.makedirs(main_dir)
+    for layer in range(len(voxelxise_correlations)):
+        np.save(os.path.join(str(main_dir), f"joint_model_prediction_voxelwise_correlation_layer{layer}"),
+                voxelxise_correlations[layer])
+
+
 if __name__ == '__main__':
     import argparse
 
@@ -98,108 +190,5 @@ if __name__ == '__main__':
     parser.add_argument("output_dir", help="Output directory", type=str)
     args = parser.parse_args()
 
-    context_representations = np.load(args.context_representations, allow_pickle=True)
-
-    training_story_names = ['alternateithicatom', 'avatar', 'howtodraw', 'legacy',
-                            'life', 'myfirstdaywiththeyankees', 'naked',
-                            'odetostepfather', 'souls', 'undertheinfluence']
-    testing_story_names = ['wheretheressmoke']
-    all_story_names = training_story_names + testing_story_names
-
-    grids = load_grids_for_stories(all_story_names)
-
-    # Load TRfiles
-    trfiles = load_generic_trfiles(all_story_names, root="stimuli/trfiles")
-
-    # Make word and phoneme datasequences
-    word_data_sequences = make_word_ds(grids, trfiles)  # dictionary of {storyname : word DataSequence}
-    eng1000 = SemanticModel.load(os.path.join(args.data_dir, "english1000sm.hf5"))
-
-    semantic_sequence_representations = dict()  # dictionary to hold projected stimuli {story name : projected DataSequence}
-    for i in np.arange(len(all_story_names)):
-        print(all_story_names[i])
-        semantic_sequence_representations[all_story_names[i]] = []
-        for layer in np.arange(args.layers):
-            temp = make_semantic_model(word_data_sequences[all_story_names[i]], [eng1000], [985])
-            temp.data = np.nan_to_num(context_representations.item()[all_story_names[i]][layer])
-            semantic_sequence_representations[all_story_names[i]].append(temp)
-
-    # Downsample stimuli, since fMRI data covers multiple words in one TR
-    interpolation_type = "lanczos"  # filter type used for averaging across representations
-    window = 3  # number of lobes in Lanczos filter
-    # num_layers = 12
-    down_sampled_semantic_sequences = dict()  # dictionary to hold down-sampled stimuli
-    for story in all_story_names:
-        down_sampled_semantic_sequences[story] = []
-        for layer in np.arange(args.layers):
-            temp = semantic_sequence_representations[story][layer].chunksums(interpolation_type, window=window)
-            down_sampled_semantic_sequences[story].append(temp)
-
-    trim = 5
-    training_stim = {}
-    prediction_stim = {}
-    for layer in np.arange(args.layers):
-        training_stim[layer] = []
-        training_stim[layer].append(
-            np.vstack(
-                [zscore(down_sampled_semantic_sequences[story][layer][5 + trim:-trim]) for story in
-                 training_story_names]))
-
-    for layer in np.arange(args.layers):
-        prediction_stim[layer] = []
-        prediction_stim[layer].append(
-            np.vstack(
-                [zscore(down_sampled_semantic_sequences[story][layer][5 + trim:-trim]) for story in
-                 testing_story_names]))
-    story_lengths = [len(down_sampled_semantic_sequences[story][0][5 + trim:-trim]) for story in training_story_names]
-    print(story_lengths)
-
-    # Delay stimuli to account for hemodynamic lag
-    numer_of_delays = 6
-    delays = range(1, numer_of_delays + 1)
-
-    print("FIR model delays: ", delays)
-    print(np.array(training_stim[0]).shape)
-    delayed_Rstim = []
-    for layer in np.arange(args.layers):
-        delayed_Rstim.append(make_delayed(np.array(training_stim[layer])[0], delays))
-
-    delayed_Pstim = []
-    for layer in np.arange(args.layers):
-        delayed_Pstim.append(make_delayed(np.array(prediction_stim[layer])[0], delays))
-
-    # Print the sizes of these matrices
-    print("delayed_Rstim shape: ", delayed_Rstim[0].shape)
-    print("delayed_Pstim shape: ", delayed_Pstim[0].shape)
-
-    # join input features (context representations and low-level textual features)
-    base_features_train, base_features_val = load_low_level_textual_features()
-
-    trim = 5
-    np.random.seed(9)
-    z_base_feature_train = np.vstack(
-        [zscore(base_features_train[story][args.low_level_feature][5 + trim:-trim]) for story in
-         base_features_train.keys()])
-    z_base_feature_val = np.vstack(
-        [zscore(base_features_val[story][args.low_level_feature][5 + trim:-trim]) for story in
-         base_features_val.keys()])
-    print("base features train shape: ", np.shape(z_base_feature_train))
-    print("base features val shape: ", np.shape(z_base_feature_val))
-
-    # join input features (context representations and low-level textual features)
-    Rstim = [np.hstack((delayed_Rstim[layer_number], z_base_feature_train)) for layer_number in np.arange(args.layers)]
-    Pstim = [np.hstack([delayed_Pstim[layer_number], z_base_feature_val]) for layer_number in np.arange(args.layers)]
-
-    subject = f'0{args.subjectNum}'
-
-    voxelxise_correlations = prediction_joint_model(Rstim, Pstim, args.data_dir, subject, args.modality,
-                                                    args.layers)
-
-    # save voxelwise correlations and predictions
-    main_dir = os.path.join(args.output_dir, args.modality, subject, args.low_level_feature)
-    if not os.path.exists(main_dir):
-        os.makedirs(main_dir)
-
-    for layer in range(len(voxelxise_correlations)):
-        np.save(os.path.join(str(main_dir), f"joint_model_prediction_voxelwise_correlation_layer{layer}"),
-                voxelxise_correlations[layer])
+    predict_joint_model(args.data_dir, args.context_representations, args.subjectNum, args.modality, args.layers,
+                        args.low_level_feature, args.output_dir)
