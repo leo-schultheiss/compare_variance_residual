205df69fafa880329b1c53a6bab4a89fbf557194
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-13 15:15:20 +0100

    rename package to robustness_test

--- robustness_test/low_level_feature_prediction.py
+++ robustness_test/low_level_feature_prediction.py
@@ -5,9 +5,7 @@ import numpy as np
 from common_utils.training_utils import load_z_low_level_feature, run_regression_and_predict, make_delayed
 
 
-def train_low_level_model(data_dir, subject_num, modality, low_level_feature, output_dir, numer_of_delays=6):
-    # Delay stimuli to account for hemodynamic lag
-    delays = range(1, numer_of_delays + 1)
+def train_low_level_model(data_dir, subject_num, modality, low_level_feature, output_dir, numer_of_delays=4):
     Rstim, Pstim = load_z_low_level_feature(data_dir, low_level_feature)
     print(f"Rstim shape: {Rstim.shape}\nPstim shape: {Pstim.shape}")
 
@@ -16,7 +14,7 @@ def train_low_level_model(data_dir, subject_num, modality, low_level_feature, ou
     delayed_Rstim = make_delayed(np.array(Rstim), delays)
     delayed_Pstim = make_delayed(np.array(Pstim), delays)
 
-
+    print(f"delayed_Rstim shape: {delayed_Rstim.shape}\ndelayed_Pstim shape: {delayed_Pstim.shape}")
     subject = f'0{subject_num}'
     voxelwise_correlations = run_regression_and_predict(delayed_Rstim, delayed_Pstim, data_dir, subject,
                                                         modality)
@@ -42,23 +40,25 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
 
-    train_low_level_model(args.data_dir, args.subject_num, args.modality, args.low_level_feature, args.output_dir, 0)
+    # train_low_level_model(args.data_dir, args.subject_num, args.modality, args.low_level_feature, args.output_dir)
 
-    # processes = []
-    # import multiprocessing
-    #
-    # for i in range(1, 15):
-    #     p = multiprocessing.Process(target=train_low_level_model, args=(
-    #         args.data_dir, args.subject_num, args.modality, args.low_level_feature, args.output_dir, i))
-    #     p.start()
-    #     processes.append(p)
-    #
-    # for p in processes:
-    #     p.join()
-    #
-    # for i in range(1, 15):
-    #     main_dir = os.path.join(args.output_dir, args.modality, f'0{args.subject_num}', args.low_level_feature)
-    #     voxelwise_correlations = np.load(
-    #         os.path.join(str(main_dir), f"low_level_model_prediction_voxelwise_correlation_delays{i}.npy"))
-    #     print(
-    #         f"Voxelwise correlation for {args.low_level_feature} with {i} delays: {np.nan_to_num(voxelwise_correlations).mean()}")
+    processes = []
+    import multiprocessing
+
+    low_level_features = ["letters", "numletters", "numphonemes", "numwords", "phonemes", "word_length_std"]
+
+    for low_level_feature in low_level_features:
+        p = multiprocessing.Process(target=train_low_level_model, args=(
+            args.data_dir, args.subject_num, args.modality, low_level_feature, args.output_dir))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+    for low_level_feature in low_level_features:
+        main_dir = os.path.join(args.output_dir, args.modality, f'0{args.subject_num}', args.low_level_feature)
+        voxelwise_correlations = np.load(
+            os.path.join(str(main_dir), f"low_level_model_prediction_voxelwise_correlation.npy"))
+        print(
+            f"Average correlation for {args.low_level_feature}: {np.nan_to_num(voxelwise_correlations).mean()}")
