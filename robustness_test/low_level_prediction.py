e4cd89bf71ee566dce6cbc3b062f65246a3c6a92
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-17 15:59:10 +0100

    rename files to more accurately depict their purpose

--- robustness_test/low_level_prediction.py
+++ robustness_test/low_level_prediction.py
@@ -15,11 +15,11 @@ def train_low_level_model(data_dir, subject_num, modality, low_level_feature, ou
     delayed_Pstim = make_delayed(np.array(Pstim), delays)
 
     print(f"delayed_Rstim shape: {delayed_Rstim.shape}\ndelayed_Pstim shape: {delayed_Pstim.shape}")
-    subject = f'0{subject_num}'
-    voxelwise_correlations = run_regression_and_predict(delayed_Rstim, delayed_Pstim, data_dir, subject,
+    voxelwise_correlations = run_regression_and_predict(delayed_Rstim, delayed_Pstim, data_dir, subject_num,
                                                         modality)
+
     # save voxelwise correlations and predictions
-    main_dir = os.path.join(output_dir, modality, subject)
+    main_dir = os.path.join(output_dir, modality, f'0{subject_num}')
     if not os.path.exists(main_dir):
         os.makedirs(main_dir)
     np.save(os.path.join(str(main_dir), f"{low_level_feature}"),
@@ -28,9 +28,8 @@ def train_low_level_model(data_dir, subject_num, modality, low_level_feature, ou
 
 if __name__ == '__main__':
     import argparse
-
-    parser = argparse.ArgumentParser(description="CheXpert NN argparser")
-    parser.add_argument("-d", "--data_dir", help="Directory containing data", type=str, default="data")
+    parser = argparse.ArgumentParser(description="Train low level feature prediction model")
+    parser.add_argument("-d", "--data_dir", help="Directory containing data", type=str, default="../data")
     parser.add_argument("-s", "--subject_num", help="Subject number", type=int, required=True)
     parser.add_argument("-m", "--modality", help="Choose modality", type=str, default="reading")
     parser.add_argument("--low_level_feature", help="Low level feature to use. Possible options include:\n"
@@ -40,25 +39,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
 
-    train_low_level_model(args.data_dir, args.subject_num, args.modality, args.low_level_feature, args.output_dir)
+    # train_low_level_model(args.data_dir, args.subject_num, args.modality, args.low_level_feature, args.output_dir)
 
-    # processes = []
-    # import multiprocessing
-    #
-    # low_level_features = ["letters", "numletters", "numphonemes", "numwords", "phonemes", "word_length_std"]
-    #
-    # for low_level_feature in low_level_features:
-    #     p = multiprocessing.Process(target=train_low_level_model, args=(
-    #         args.data_dir, args.subject_num, args.modality, low_level_feature, args.output_dir))
-    #     p.start()
-    #     processes.append(p)
-    #
-    # for p in processes:
-    #     p.join()
-    #
-    # for low_level_feature in low_level_features:
-    #     main_dir = os.path.join(args.output_dir, args.modality, f'0{args.subject_num}', low_level_feature)
-    #     voxelwise_correlations = np.load(
-    #         os.path.join(str(main_dir), f"low_level_model_prediction_voxelwise_correlation.npy"))
-    #     print(
-    #         f"Average correlation for {low_level_feature}: {np.nan_to_num(voxelwise_correlations).mean()}")
+    processes = []
+    import multiprocessing
+
+    low_level_features = ["letters", "numletters", "numphonemes", "numwords", "phonemes", "word_length_std"]
+
+    for low_level_feature in low_level_features:
+        p = multiprocessing.Process(target=train_low_level_model, args=(
+            args.data_dir, args.subject_num, args.modality, low_level_feature, args.output_dir))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+    print("All processes finished")
