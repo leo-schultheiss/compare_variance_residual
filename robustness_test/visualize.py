86ecfefc9aeb86a3c000157a35d6fd5491edd0fc
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-18 09:09:29 +0100

    modularize visualize.py

--- robustness_test/visualize.py
+++ robustness_test/visualize.py
@@ -4,9 +4,9 @@ import numpy as np
 
 from robustness_test.common_utils.visualization import flatmap_subject_voxel_data, plot_model_comparison_rois, plot_model_histogram_comparison
 
-modality = "reading"
+modality = "listening"
 subject = "01"
-layer = 11
+layer = 9
 low_level_feature = "letters"
 
 standard_correlation = np.load(os.path.join("../bert-predictions", modality, subject, f"layer_{layer}.npy"), allow_pickle=True)
@@ -16,11 +16,12 @@ intersection = np.load("../bert-variance-partitioning/intersection.npy", allow_p
 standard_minus_low_level = np.load("../bert-variance-partitioning/variance_a_minus_b.npy", allow_pickle=True)
 low_level_minus_standard = np.load("../bert-variance-partitioning/variance_b_minus_a.npy", allow_pickle=True)
 
-print(f"minimum intersection: {np.sum(np.nan_to_num(intersection) == 0)}")
+# relace nans with zeros
+standard_correlation = np.nan_to_num(standard_correlation)
+low_level_correlation = np.nan_to_num(low_level_correlation)
+joint_correlation = np.nan_to_num(joint_correlation)
+intersection = np.nan_to_num(intersection)
+standard_minus_low_level = np.nan_to_num(standard_minus_low_level)
+low_level_minus_standard = np.nan_to_num(low_level_minus_standard)
 
-flatmap_subject_voxel_data(subject, intersection)
-flatmap_subject_voxel_data(subject, standard_minus_low_level)
-flatmap_subject_voxel_data(subject, low_level_minus_standard)
-
-plot_model_histogram_comparison(standard_correlation, standard_minus_low_level, "semantic", "semantic \ low level")
-
+plot_model_histogram_comparison(standard_correlation, joint_correlation, "semantic", "joint")
