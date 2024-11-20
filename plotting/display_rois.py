171f32ad559e8af506e7936f5ab7fc3534513f47
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-20 09:39:51 +0100

    move visualization to plotting

--- plotting/display_rois.py
+++ plotting/display_rois.py
@@ -3,32 +3,34 @@ from matplotlib import pyplot as plt
 from voxelwise_tutorials.io import load_hdf5_array
 
 import os
-from robustness_test.common_utils.visualization import flatmap_subject_voxel_data
 import voxelwise_tutorials.viz as viz
 
 language_model = "bert"
-modality = "reading"
+modality = "listening"
 subject = "01"
 layer = 9
-feature = "low-level" # "low-level" "joint"
-low_level_feature = "numletters"
+feature = "joint" # semantic low-level joint
+low_level_feature = "word_length_std"
 
 
 filename = f"{low_level_feature}.npy" if feature == "low-level" else f"layer_{layer}.npy"
 joint_path_addition = f"{low_level_feature}" if feature == "joint" else ""
 path = os.path.join(f"../{language_model}-{feature}-predictions", modality, subject, joint_path_addition, filename)
 print("loading", path)
-correlation = np.load(path, allow_pickle=True)
+correlation = np.nan_to_num(np.load(path, allow_pickle=True))
 mapper = os.path.join("../data", f"subject{subject}_mappers.hdf")
-
-# flatmap_subject_voxel_data(subject_number=subject, data=correlation)
 
 flatmap_mask = load_hdf5_array(mapper, key='flatmap_mask')
 figsize = np.array(flatmap_mask.shape) / 100.
 fig = plt.figure(figsize=figsize)
 ax = fig.add_axes((0, 0, 1, 1))
 ax.axis('off')
-viz.plot_flatmap_from_mapper(correlation, mapper, ax=ax)
+viz.plot_flatmap_from_mapper(correlation, mapper, ax=ax, with_curvature=False, alpha=1, cmap='inferno')
 
-fig.suptitle(f"{feature} - Subject {subject} - Layer {layer} - {modality} - {low_level_feature}")
+layer_info = "" if feature == "low-level" else f" - layer {layer}"
+low_level_info = "" if feature == "semantic" else f" - {low_level_feature}"
+figure_name = f"{feature} - subject {subject} - {modality}{low_level_info}{layer_info}"
+
+fig.suptitle(figure_name)
 fig.show()
+fig.savefig(f"../plots/{figure_name}")
