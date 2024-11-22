6b23aa9769b476176d0d4f8f54d71049cf65a25f
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-22 14:27:57 +0100

    start brain maps always at 0

--- plotting/display_rois.py
+++ plotting/display_rois.py
@@ -5,18 +5,16 @@ from voxelwise_tutorials.io import load_hdf5_array
 import os
 import voxelwise_tutorials.viz as viz
 
+from robustness_test.common_utils.training_utils import get_prediction_path
+
 language_model = "bert"
+feature = "low-level"
 modality = "listening"
 subject = "01"
 layer = 9
-feature = "joint" # semantic low-level joint
-low_level_feature = "word_length_std"
+low_level_feature = "phonemes"
 
-
-filename = f"{low_level_feature}.npy" if feature == "low-level" else f"layer_{layer}.npy"
-joint_path_addition = f"{low_level_feature}" if feature == "joint" else ""
-path = os.path.join(f"../{language_model}-{feature}-predictions", modality, subject, joint_path_addition, filename)
-print("loading", path)
+path = get_prediction_path(language_model, feature, modality, subject, low_level_feature, layer)
 correlation = np.nan_to_num(np.load(path, allow_pickle=True))
 mapper = os.path.join("../data", f"subject{subject}_mappers.hdf")
 
@@ -25,12 +23,12 @@ figsize = np.array(flatmap_mask.shape) / 100.
 fig = plt.figure(figsize=figsize)
 ax = fig.add_axes((0, 0, 1, 1))
 ax.axis('off')
-viz.plot_flatmap_from_mapper(correlation, mapper, ax=ax, with_curvature=False, alpha=1, cmap='inferno')
+viz.plot_flatmap_from_mapper(correlation, mapper, ax=ax, with_curvature=False, alpha=1, cmap='inferno', vmin=0, vmax=np.max(correlation))
 
 layer_info = "" if feature == "low-level" else f" - layer {layer}"
 low_level_info = "" if feature == "semantic" else f" - {low_level_feature}"
 figure_name = f"{feature} - subject {subject} - {modality}{low_level_info}{layer_info}"
 
 fig.suptitle(figure_name)
-fig.show()
 fig.savefig(f"../plots/{figure_name}")
+fig.show()
