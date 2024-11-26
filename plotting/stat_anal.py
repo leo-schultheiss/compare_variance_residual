a4e80c35ee77f3b651c05124944cd3825562272a
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-26 13:55:30 +0100

    add alternative title for stat_anal.py

--- plotting/stat_anal.py
+++ plotting/stat_anal.py
@@ -3,22 +3,15 @@ from matplotlib import pyplot as plt
 
 import os
 
-language_model = "bert"
-feature = "semantic"  # semantic low-level joint
-modality = "listening"
-subject = "01"
-layer = 9
-low_level_feature = "phonemes"
 
-
-def correlation_stat_analysis(correlation, savefig=False):
+def correlation_stat_analysis(correlation, savefig=False, alternate_title=None):
     # print statistics
+    print("number of voxels", len(correlation))
     print("mean", np.mean(correlation))
-    print("median", np.median(correlation))
     print("std", np.std(correlation))
     print("min", np.min(correlation))
     print("max", np.max(correlation))
-    print("sum", np.sum(correlation))
+    print("median", np.median(correlation))
     # plot histogram with inferno cmap
     # This is the colormap I'd like to use.
     cm = plt.colormaps['inferno']
@@ -30,10 +23,13 @@ def correlation_stat_analysis(correlation, savefig=False):
     col /= max(col)
     for c, p in zip(col, patches):
         plt.setp(p, 'facecolor', cm(c))
-    if feature != "semantic":
-        plt.suptitle(f"{feature} {modality} {low_level_feature}")
+    if alternate_title:
+        plt.suptitle(alternate_title)
     else:
-        plt.suptitle(f"{feature} {modality}")
+        if feature != "semantic":
+            plt.suptitle(f"{feature} {modality} {low_level_feature}")
+        else:
+            plt.suptitle(f"{feature} {modality}")
     plt.xlabel("Correlation")
     plt.ylabel("Number of Voxels")
     left, right = plt.xlim()
@@ -49,6 +45,13 @@ def correlation_stat_analysis(correlation, savefig=False):
 
 
 if __name__ == "__main__":
+    language_model = "bert"
+    feature = "joint"  # semantic low-level joint
+    modality = "reading"
+    subject = "01"
+    layer = 9
+    low_level_feature = "letters"
+
     # Load the data
     filename = f"{low_level_feature}.npy" if feature == "low-level" else f"layer_{layer}.npy"
     joint_path_addition = f"{low_level_feature}" if feature == "joint" else ""
