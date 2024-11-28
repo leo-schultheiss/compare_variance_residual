2f01a844f72d89bba56424d299d32c9ab6b59d77
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-28 15:13:20 +0100

    use RidgeCV for low level predictions

--- robustness_test/low_level_prediction.py
+++ robustness_test/low_level_prediction.py
@@ -1,11 +1,15 @@
 import os.path
 
 import numpy as np
+from himalay.ridge import RidgeCV
+from ridge_utils.util import make_delayed
 
-from common_utils.training_utils import load_z_low_level_feature, run_regression_and_predict, make_delayed
+from common_utils.training_utils import load_z_low_level_feature, load_subject_fmri
+from robustness_test.common_utils.training_utils import get_prediction_path
 
 
 def train_low_level_model(data_dir, subject_num, modality, low_level_feature, output_dir, numer_of_delays=4):
+    Rresp, Presp = load_subject_fmri(data_dir, subject_num, modality)
     Rstim, Pstim = load_z_low_level_feature(data_dir, low_level_feature)
     print(f"Rstim shape: {Rstim.shape}\nPstim shape: {Pstim.shape}")
 
@@ -15,19 +19,22 @@ def train_low_level_model(data_dir, subject_num, modality, low_level_feature, ou
     delayed_Pstim = make_delayed(np.array(Pstim), delays)
 
     print(f"delayed_Rstim shape: {delayed_Rstim.shape}\ndelayed_Pstim shape: {delayed_Pstim.shape}")
-    voxelwise_correlations = run_regression_and_predict(delayed_Rstim, delayed_Pstim, data_dir, subject_num,
-                                                        modality)
+    model = RidgeCV(alphas=np.logspace(0, 3, 10), cv=5)
+    model.fit(Rstim, Rresp)
+    voxelwise_correlations = model.score(Pstim, Presp)
 
     # save voxelwise correlations and predictions
-    main_dir = os.path.join(output_dir, modality, f'0{subject_num}')
-    if not os.path.exists(main_dir):
-        os.makedirs(main_dir)
-    np.save(os.path.join(str(main_dir), f"{low_level_feature}"),
-            voxelwise_correlations)
+    output_file = get_prediction_path(language_model=None, feature="low-level", modality=modality, subject=subject_num,
+                                      low_level_feature=low_level_feature)
+    output_dir = os.path.dirname(output_file)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    np.save(output_file, voxelwise_correlations)
 
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser(description="Train low level feature prediction model")
     parser.add_argument("-d", "--data_dir", help="Directory containing data", type=str, default="../data")
     parser.add_argument("-s", "--subject_num", help="Subject number", type=int, default=1)
@@ -42,7 +49,6 @@ if __name__ == '__main__':
     # train_low_level_model(args.data_dir, args.subject_num, args.modality, args.low_level_feature, args.output_dir)
 
     processes = []
-    import multiprocessing
 
     low_level_features = ["letters", "numletters", "numphonemes", "numwords", "phonemes", "word_length_std"]
 
