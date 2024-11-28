9dbedd0b126216ac2320543d9b171e9411b1d105
Leo Schultheiss <leo.schultheiss@tum.de>
2024-11-28 14:26:27 +0100

    use language model as input to determine output file path automatically

--- robustness_test/semantic_prediction.py
+++ robustness_test/semantic_prediction.py
@@ -1,17 +1,18 @@
-import numpy as np
 import argparse
-
 import os
-from himalaya.ridge import RidgeCV, Ridge
+
+import numpy as np
+from himalaya.ridge import RidgeCV
 from ridge_utils.util import make_delayed
 
-from robustness_test.common_utils.training_utils import load_context_representations_interpolated, load_subject_fmri
+from robustness_test.common_utils.training_utils import load_downsampled_context_representations, load_subject_fmri, \
+    get_prediction_path
 
 trim = 5
 
 
-def predict_brain_activity(data_dir: str, feature_filename: str, layer: int, subject_num: int, modality: str,
-                           output_directory: str):
+def predict_brain_activity(data_dir: str, feature_filename: str, language_model: str, layer: int, subject_num: int,
+                           modality: str):
     """
     Predict brain activity using semantic representations of words
     :param data_dir: data directory
@@ -19,10 +20,10 @@ def predict_brain_activity(data_dir: str, feature_filename: str, layer: int, sub
     :param layer: layer of natural language model to use for semantic representation of words
     :param subject_num: subject number
     :param modality: modality 'reading' or 'listening'
-    :param output_directory: output directory
     """
     # Load data
     predicion_stim, training_stim = load_downsampled_context_representations(data_dir, feature_filename, layer)
+    zRresp, zPresp = load_subject_fmri(data_dir, subject_num, modality)
 
     # Delay stimuli
     numer_of_delays = 4
@@ -32,35 +33,36 @@ def predict_brain_activity(data_dir: str, feature_filename: str, layer: int, sub
     delayed_Rstim = make_delayed(np.array(training_stim), delays)
     delayed_Pstim = make_delayed(np.array(predicion_stim), delays)
 
-    zRresp, zPresp = load_subject_fmri(data_dir, subject_num, modality)
-
     print("Rstim.shape: ", delayed_Rstim.shape)
     print("Rresp.shape: ", zRresp.shape)
     print("Pstim.shape: ", delayed_Pstim.shape)
     print("Presp.shape: ", zPresp.shape)
 
     # Run regression
-    model = Ridge(alpha=np.logspace(0, 4, 10))
+    model = RidgeCV(alphas=np.logspace(1, 3, 10))
     model.fit(delayed_Rstim, zRresp)
-    voxcorrs = model.score(delayed_Pstim, zPresp)
+    voxelwise_correlations = model.score(delayed_Pstim, zPresp)
 
-    main_dir = os.path.join(output_directory, modality, f'{subject_num:02}')
-    if not os.path.exists(main_dir):
-        os.makedirs(main_dir)
-    np.save(os.path.join(str(main_dir), "layer_" + str(layer)), voxcorrs)
+    # save results
+    output_file = get_prediction_path(language_model, "semantic", modality, subject_num, layer=layer)
+    output_directory = os.path.dirname(output_file)
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    np.save(output_file, voxelwise_correlations)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Predict brain activity")
     parser.add_argument("--data_dir", help="Choose data directory", type=str, default="../data")
     parser.add_argument("--feature_filename", help="Choose feature", type=str, default="../bert_base20.npy")
+    parser.add_argument("--language_model", help="Choose language model", type=str, default="bert")
     parser.add_argument("--layer", help="Layer of natural language model to use for semantic representation of words",
                         type=int, default=9)
     parser.add_argument("--subject_num", help="Choose subject", type=int, default=1)
     parser.add_argument("--modality", help="Choose modality", type=str, default="reading")
-    parser.add_argument("--dirname", help="Choose Directory", type=str, default="../predictions/bert/semantic")
     args = parser.parse_args()
     print(args)
 
-    predict_brain_activity(args.data_dir, args.feature_filename, args.layer, args.subject_num, args.modality,
-                           args.dirname)
+    predict_brain_activity(data_dir=args.data_dir, feature_filename=args.feature_filename,
+                           language_model=args.language_model, layer=args.layer, subject_num=args.subject_num,
+                           modality=args.modality)
