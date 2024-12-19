48e19daf6951ce2d8c1bf7605e6457f77db516cc
Leo <leo.schultheiss@tum.de>
2024-12-19 15:40:59 +0100

    create separate method for residual

---
+++ compare_variance_residual/simulated/residual.py
@@ -0,0 +1,34 @@
+import himalaya.backend
+import numpy as np
+from himalaya.kernel_ridge import KernelRidgeCV
+from sklearn.linear_model import LinearRegression
+
+
+def residual_method(Xs_train, Xs_test, Y_train, Y_test, use_ols=False):
+    backend = himalaya.backend.get_backend()
+
+    # train model for creating residuals
+    if use_ols:
+        model = LinearRegression()
+        feature, target = backend.to_numpy(Xs_train[1]), backend.to_numpy(Xs_train[0])
+        model.fit(feature, target)
+        train_predict = model.predict(backend.to_numpy(Xs_train[1]))
+        test_predict = model.predict(backend.to_numpy(Xs_test[1]))
+    else:
+        model = KernelRidgeCV(alphas=np.logspace(-10, 10, 41), kernel="linear", solver="eigenvalues")
+        model.fit(Xs_train[1], Xs_train[0])
+        train_predict = model.predict(Xs_train[1])
+        test_predict = model.predict(Xs_test[1])
+
+    train_predict, test_predict = backend.asarray(train_predict), backend.asarray(test_predict)
+
+    train_residual = Xs_train[0] - train_predict
+    test_residual = Xs_test[0] - test_predict
+
+    model_residual = KernelRidgeCV(alphas=np.logspace(-10, 10, 41), kernel="linear")
+    model_residual.fit(train_residual, Y_train)
+
+    score = model_residual.score(test_residual, Y_test)
+    score = backend.to_numpy(score)
+    mean = np.mean(score)
+    return mean
--- compare_variance_residual/simulated/simulation.py
+++ compare_variance_residual/simulated/simulation.py
@@ -5,7 +5,7 @@ from himalaya.backend import get_backend
 def generate_dataset(n_targets=500,
                      n_samples_train=1000, n_samples_test=400,
                      noise=0, unique_contributions=None,
-                     n_features_list=None, random_state=None, random_distribution=np.random.randn):
+                     n_features_list=None, random_distribution=np.random.randn, random_state=None):
     """Utility to generate dataset.
 
     Parameters
@@ -22,6 +22,9 @@ def generate_dataset(n_targets=500,
         Proportion of the target variance explained by each feature space.
     n_features_list : list of int of length (n_features, ) or None
         Number of features in each kernel. If None, use 1000 features for each.
+    random_distribution : callable
+        Function to generate random features.
+        Should support the signature (n_samples, n_features) -> array of shape (n_samples, n_features).
     random_state : int, or None
         Random generator seed use to generate the true kernel weights.
 
@@ -40,8 +43,7 @@ def generate_dataset(n_targets=500,
     n_features_list : list of int of length (n_features, )
         Number of features in each kernel.
     """
-    from himalaya.utils import check_random_state
-
+    np.random.seed(random_state)
     backend = get_backend()
 
     if unique_contributions is None:
@@ -50,36 +52,25 @@ def generate_dataset(n_targets=500,
     if n_features_list is None:
         n_features_list = np.full(len(unique_contributions), fill_value=1000)
 
-    rng = check_random_state(random_state)
-
     # Then, generate a random dataset, using the arbitrary scalings.
     Xs_train, Xs_test = [], []
-    Xs_unique_test, Xs_unique_train = [], []
     Y_train, Y_test = None, None
 
-    # Generate a shared component Z
-    Z_train = rng.randn(n_samples_train, 1)
-    Z_test = rng.randn(n_samples_test, 1)
+    # Generate a shared component
+    S_train = random_distribution(n_samples_train, 1)
+    S_test = random_distribution(n_samples_test, 1)
 
     for ii, unique_contribution in enumerate(unique_contributions):
         n_features = n_features_list[ii]
-        weights = rng.randn(n_features, n_targets) / n_features
 
         # generate random features
-        X_train = rng.randn(n_samples_train, n_features)
-        X_test = rng.randn(n_samples_test, n_features)
-
-        # compute unique feature contribution, before adding shared component
-        X_unique_train = unique_contribution * X_train @ weights
-        X_unique_test = unique_contribution * X_test @ weights
-        Xs_unique_train.append(X_unique_train)
-        Xs_unique_test.append(X_unique_test)
+        X_train = random_distribution(n_samples_train, n_features)
+        X_test = random_distribution(n_samples_test, n_features)
 
         # add shared component
         shared_contribution = (1 - np.sum(unique_contributions)) / len(unique_contributions)
-        print("shared_contribution", shared_contribution)
-        X_train = shared_contribution * Z_train + unique_contribution * X_train
-        X_test = shared_contribution * Z_test + unique_contribution * X_test
+        X_train = shared_contribution * S_train + unique_contribution * X_train
+        X_test = shared_contribution * S_test + unique_contribution * X_test
 
         # demean
         X_train -= X_train.mean(0)
@@ -87,6 +78,8 @@ def generate_dataset(n_targets=500,
 
         Xs_train.append(X_train)
         Xs_test.append(X_test)
+
+        weights = random_distribution(n_features, n_targets) / n_features
 
         if ii == 0:
             Y_train = X_train @ weights
@@ -99,16 +92,15 @@ def generate_dataset(n_targets=500,
     Y_train /= std
     Y_test /= std
 
-    Y_train += rng.randn(n_samples_train, n_targets) * noise
-    Y_test += rng.randn(n_samples_test, n_targets) * noise
+    Y_train += random_distribution(n_samples_train, n_targets) * noise
+    Y_test += random_distribution(n_samples_test, n_targets) * noise
     Y_train -= Y_train.mean(0)
     Y_test -= Y_test.mean(0)
+
 
     Xs_train = backend.asarray(Xs_train, dtype="float32")
     Xs_test = backend.asarray(Xs_test, dtype="float32")
     Y_train = backend.asarray(Y_train, dtype="float32")
     Y_test = backend.asarray(Y_test, dtype="float32")
-    # Xs_unique_train = backend.asarray(Xs_unique_train, dtype="float32")
-    # Xs_unique_test = backend.asarray(Xs_unique_test, dtype="float32")
 
-    return Xs_train, Xs_test, Y_train, Y_test, n_features_list, Xs_unique_train, Xs_unique_test
+    return Xs_train, Xs_test, Y_train, Y_test, n_features_list
