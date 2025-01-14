28723a803b40d6202186c5de26b4ff7c4241f38a
Leo <leo.schultheiss@tum.de>
2025-01-14 09:59:38 +0100

    add parameters for cv, alpha and whether to use direct or normal approach in variance partitioning

--- compare_variance_residual/simulated/residual.py
+++ compare_variance_residual/simulated/residual.py
@@ -1,10 +1,10 @@
 import himalaya.backend
 import numpy as np
-from himalaya.kernel_ridge import KernelRidgeCV
+from himalaya.ridge import RidgeCV
 from sklearn.linear_model import LinearRegression
 
 
-def residual_method(Xs_train, Xs_test, Y_train, Y_test, use_ols=False, ignore_negative_r2=False):
+def residual_method(Xs_train, Xs_test, Y_train, Y_test, alphas=np.logspace(-10, 10, 41), cv=10,use_ols=False, ignore_negative_r2=False):
     backend = himalaya.backend.get_backend()
 
     # train model for creating residuals
@@ -15,7 +15,8 @@ def residual_method(Xs_train, Xs_test, Y_train, Y_test, use_ols=False, ignore_ne
         train_predict = model.predict(backend.to_numpy(Xs_train[1]))
         test_predict = model.predict(backend.to_numpy(Xs_test[1]))
     else:
-        model = KernelRidgeCV(alphas=np.logspace(-10, 10, 41), kernel="linear", solver="eigenvalues", warn=False)
+        solver_params = dict(warn=False)
+        model = RidgeCV(alphas=alphas, cv=cv, solver_params=solver_params)
         model.fit(Xs_train[1], Xs_train[0])
         train_predict = model.predict(Xs_train[1])
         test_predict = model.predict(Xs_test[1])
@@ -25,7 +26,8 @@ def residual_method(Xs_train, Xs_test, Y_train, Y_test, use_ols=False, ignore_ne
     train_residual = Xs_train[0] - train_predict
     test_residual = Xs_test[0] - test_predict
 
-    model_residual = KernelRidgeCV(alphas=np.logspace(-10, 10, 41), kernel="linear", warn=False)
+    solver_params = dict(warn=False)
+    model_residual = RidgeCV(alphas=alphas, cv=cv, solver_params=solver_params)
     model_residual.fit(train_residual, Y_train)
 
     score = model_residual.score(test_residual, Y_test)
