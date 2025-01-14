54a9cfc2f5747cb81dc6929484ca1031714406da
Leo <leo.schultheiss@tum.de>
2025-01-14 19:27:35 +0100

    move all package related files in compare_variance_residual

---
+++ compare_variance_residual/fmri_comparison/plotting/kernel_density.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as pl
+import scipy.stats as st
+
+def kde_plot(x, y, xmin=0, xmax=1, ymin=0, ymax=1):
+    # Peform the kernel density estimate
+    xx, yy = np.mgrid[xmin:xmax:100j, ymin:ymax:100j]
+    positions = np.vstack([xx.ravel(), yy.ravel()])
+    values = np.vstack([x, y])
+    kernel = st.gaussian_kde(values)
+    f = np.reshape(kernel(positions).T, xx.shape)
+
+    fig = pl.figure()
+    ax = fig.gca()
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+    # Contourf plot
+    # cfset = ax.contourf(xx, yy, f, cmap='Blues')
+    ## Or kernel density estimate plot instead of the contourf plot
+    ax.imshow(np.rot90(f), cmap='Blues', extent=[xmin, xmax, ymin, ymax])
+    # Contour plot
+    cset = ax.contour(xx, yy, f, colors='k')
+    # Label plot
+    ax.clabel(cset, inline=1, fontsize=10)
+    ax.set_xlabel('Y1')
+    ax.set_ylabel('Y0')
+
+    pl.show()
