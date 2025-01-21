784a8db9e667dd6909e3d6e85b7f3b312ed46ba9
Leo <leo.schultheiss@tum.de>
2025-01-21 15:12:08 +0100

    add results

--- compare_variance_residual/simulated/plotting.py
+++ compare_variance_residual/simulated/plotting.py
@@ -61,7 +61,7 @@ def plot_boxplots(predicted_residual, predicted_variance, title, x, x_is_log, xl
     else:
         ax.set_xlim([-0.5, len(x) - 0.5])
         ax.set_xticks(np.arange(len(x)))
-        ax.set_xticklabels(x, rotation=45, ha='right')
+        ax.set_xticklabels(format_variable_values(x), rotation=45, ha='right')
 
     if x_is_log:
         ax.set_xscale("log")
@@ -69,17 +69,17 @@ def plot_boxplots(predicted_residual, predicted_variance, title, x, x_is_log, xl
     return fig, ax
 
 
-def plot_predicted_contributions_box(xlabel, x, predicted_variance: list, predicted_residual: list,
-                                     scalars, x_is_log=False, save_dir=None, **kwargs):
-    title = "Predicted Contributions for Variance Partitioning and Residual Method"
-    ylabel = "predicted contribution"
+def plot_predicted_variances_box(xlabel, x, predicted_variance: list, predicted_residual: list,
+                                 scalars, x_is_log=False, save_dir=None, **kwargs):
+    title = "Predicted Variance"
+    ylabel = "predicted variance"
     ylim = [-0.1, 1.1]
 
     fig, ax = plot_boxplots(predicted_residual, predicted_variance, title, x, x_is_log, xlabel, ylabel, ylim)
 
     # draw center line
-    true_contribution = scalars[1]
-    ax.axhline(y=true_contribution, color='k', linestyle='--', label=r'true contribution of $X_0$')
+    true_variance = scalars[1]
+    ax.axhline(y=true_variance, color='k', linestyle='--', label='true variance')
 
     # Add legend
     ax.legend(loc='upper right')
@@ -90,7 +90,7 @@ def plot_predicted_contributions_box(xlabel, x, predicted_variance: list, predic
     plt.tight_layout()
 
     if save_dir is not None:
-        plt.savefig(os.path.join(save_dir, f"{xlabel}_predicted_contributions.png"))
+        plt.savefig(os.path.join(save_dir, f"{xlabel}_predicted_variances.png"))
 
     plt.show()
 
@@ -115,13 +115,13 @@ def plot_prediction_error(x, xlabel, predicted_variance: list, predicted_residua
 
         return smallest_whisker, largest_whisker
 
-    title = "Deviation from true contribution"
-    ylabel = "predicted contribution - true contribution"
+    title = "Deviation from true variance"
+    ylabel = "predicted variance - true variance"
 
-    # transform data to reflect error from true contribution
-    true_contribution = scalars[1]
-    predicted_variance = np.array(predicted_variance) - true_contribution
-    predicted_residual = np.array(predicted_residual) - true_contribution
+    # transform data to reflect error from true variance
+    true_variance = scalars[1]
+    predicted_variance = np.array(predicted_variance) - true_variance
+    predicted_residual = np.array(predicted_residual) - true_variance
 
     # Calculate the whiskers greatest extent
     variance_min_whisker, variance_max_whisker = calculate_whiskers(predicted_variance)
@@ -142,7 +142,7 @@ def plot_prediction_error(x, xlabel, predicted_variance: list, predicted_residua
     fig, ax = plot_boxplots(predicted_residual, predicted_variance, title, x, x_is_log, xlabel, ylabel, ylim)
 
     # draw center line
-    ax.axhline(y=0, color='k', linestyle='--', label='true contribution of $X_0$')
+    ax.axhline(y=0, color='k', linestyle='--', label='true variance')
 
     # Add legend
     ax.legend(loc='upper right')
@@ -167,10 +167,10 @@ def plot_prediction_scatter(xlabel, x, predicted_variance: list, predicted_resid
         predicted_residual = [np.clip(residual, np.percentile(residual, 5), np.percentile(residual, 95)) for residual in
                               predicted_residual]
 
-    # center data around true contribution
-    true_contribution = scalars[1]
-    predicted_variance = list(np.array(predicted_variance) - true_contribution)
-    predicted_residual = list(np.array(predicted_residual) - true_contribution)
+    # center data around true variance
+    true_variance = scalars[1]
+    predicted_variance = list(np.array(predicted_variance) - true_variance)
+    predicted_residual = list(np.array(predicted_residual) - true_variance)
 
     if normalize:
         predicted_variance = [normalize_to_unit_interval(variance) for variance in predicted_variance]
@@ -178,12 +178,14 @@ def plot_prediction_scatter(xlabel, x, predicted_variance: list, predicted_resid
 
     # Calculate the number of rows and columns needed
     n_plots = len(predicted_variance)
-    ncols = int(np.ceil(np.sqrt(n_plots)))
-    nrows = int(np.ceil(n_plots / ncols))
-    fig, ax = plt.subplots(nrows, ncols, figsize=(ncols * 4.5, nrows * 4.5), squeeze=False, sharex=normalize,
-                           sharey=normalize)
+    # ncols = int(np.ceil(np.sqrt(n_plots)))
+    # nrows = int(np.ceil(n_plots / ncols))
+    nrows = 1
+    ncols = n_plots
+    fig, ax = plt.subplots(nrows, ncols, figsize=(ncols * 4.5, nrows * 4.5), squeeze=False, sharex=True,
+                           sharey=True)
     # add title to the figure
-    fig.suptitle(f"Scatter plots over {xlabel}: Predicted Variance vs Residual Deviation from True Contribution",
+    fig.suptitle(f"Error",
                  fontsize=20, y=1.0)
 
     for i, (variance, residual) in enumerate(zip(predicted_variance, predicted_residual)):
@@ -191,25 +193,25 @@ def plot_prediction_scatter(xlabel, x, predicted_variance: list, predicted_resid
         title = f"{xlabel}: " + format_variable_values(x[i])
         ax[i // ncols, i % ncols].set_title(title)
 
+        ax[i // ncols, i % ncols].set_xlabel("predicted - true variance")
+        if i % ncols == 0:
+            ax[i // ncols, i % ncols].set_ylabel("predicted - true variance")
+
         # add text box that displays the correlation coefficient
         corr = np.corrcoef(variance, residual)[0, 1]
         ax[i // ncols, i % ncols].text(0.05, 0.95, rf"$\rho$: {corr:.2f}",
                                        transform=ax[i // ncols, i % ncols].transAxes,
                                        fontsize=10, verticalalignment='top', bbox=dict(facecolor='white', alpha=0.5))
 
-        # if normalize:
-        #     xlims, ylims = [-1.1, 1.1], [-1.1, 1.1]
-        # else:
-        #     xlims, ylims = calculate_plot_limits(residual, variance)
-        xlims = [-1.1, 1.1]
-        ylims = [-1.1, 1.1]
+        xlims = [-1.1 * true_variance, 1.1 * true_variance]
+        ylims = [-1.1 * true_variance, 1.1 * true_variance]
 
         ax[i // ncols, i % ncols].set_xlim(xlims)
         ax[i // ncols, i % ncols].set_ylim(ylims)
         # plot x=y
         ax[i // ncols, i % ncols].plot([-10000, 10000], [-10000, 10000], 'k--', label="x=y")
         # plot X=0, and y=0
-        ax[i // ncols, i % ncols].axvline(x=0, color='k', linestyle='-', label='true contribution of $X_0$')
+        ax[i // ncols, i % ncols].axvline(x=0, color='k', linestyle='-')
         ax[i // ncols, i % ncols].axhline(y=0, color='k', linestyle='-')
         # add legend
         ax[i // ncols, i % ncols].legend(loc='lower right')
@@ -219,10 +221,8 @@ def plot_prediction_scatter(xlabel, x, predicted_variance: list, predicted_resid
             ax[i // ncols, i % ncols].xaxis.set_tick_params(labelbottom=True)
 
     # add x and y labels
-    fig.text(0.5, -0.01, f"variance partitioning predicted - true contribution{', normalized' if normalize else ''}",
-             ha='center', fontsize=15)
-    fig.text(-0.01, 0.5, f"residual method predicted - true contribution{', normalized' if normalize else ''}",
-             rotation='vertical', va='center', fontsize=15)
+    fig.text(0.5, 0, f"Variance Partitioning", ha='center', va='bottom', fontsize=13)
+    fig.text(0, 0.5, f"Residual Method", rotation='vertical', va='center', ha='left', fontsize=13)
 
     # remove empty subplots
     for i in range(n_plots, nrows * ncols):
@@ -235,7 +235,7 @@ def plot_prediction_scatter(xlabel, x, predicted_variance: list, predicted_resid
     plt.tight_layout()
 
     if save_dir is not None:
-        plt.savefig(os.path.join(save_dir, f"{xlabel}_predicted_contributions_scatter.png"))
+        plt.savefig(os.path.join(save_dir, f"{xlabel}_predicted_variances_scatter.png"), pad_inches=1)
     plt.show()
 
 
@@ -244,27 +244,32 @@ def plot_mse(variable_name, variable_values, predicted_variance, predicted_resid
     figure_width = 1.5 * len(predicted_residual)
     fig, ax = plt.subplots(figsize=(figure_width, 4.5))
 
-    # calculate mse for each variable
+    # calculate and plot mse for each variable
     variance_mse = [np.mean((np.array(var) - scalars[1]) ** 2) for var in predicted_variance]
     residual_mse = [np.mean((np.array(res) - scalars[1]) ** 2) for res in predicted_residual]
-
     ax.plot(variable_values, variance_mse, label="variance partitioning")
     ax.plot(variable_values, residual_mse, label="residual method")
 
-    ax.set_title("Mean Squared Error for Variance Partitioning and Residual Method")
+    # plot y=0
+    ax.axhline(y=0, color='k', linestyle='-')
+
+    ax.set_title("Mean Squared Error")
     ax.set_xlabel(variable_name)
     ax.set_ylabel("MSE")
-
     max_total = max(np.max(variance_mse), np.max(residual_mse)) * 1.1
 
     if max_total > 1:
         ax.set_yscale("log")
         ax.yaxis.set_major_formatter(plt.LogFormatter())
 
-    ax.set_ylim([-0.01, max_total])
+    ax.set_ylim([max(-0.1 * max_total, -0.01), max_total])
+
+    if x_is_log:
+        w = 1 / (3 * len(predicted_residual))
+    else:
+        w = 0.05
 
     if isinstance(variable_values[0], (int, float)):
-        w = 1 / (2 * len(predicted_residual))
         ax.set_xticks(variable_values)
         ax.set_xticklabels(variable_values)
         ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:.2f}"))
@@ -283,6 +288,7 @@ def plot_mse(variable_name, variable_values, predicted_variance, predicted_resid
     fig.text(1, 0.5, create_text(**kwargs), ha='left', va='center',
              fontsize=10)
 
+    plt.legend()
     plt.tight_layout()
     if save_dir is not None:
         plt.savefig(os.path.join(save_dir, f"{variable_name}_mse.png"))
@@ -313,8 +319,8 @@ def calculate_plot_limits(residual, variance):
 
 def plot_experiment(variable_name, variable_values, predicted_variance, predicted_residual, scalars,
                     x_is_log=False, save_dir=None, **kwargs):
-    plot_predicted_contributions_box(variable_name, variable_values, predicted_variance, predicted_residual,
-                                     scalars, x_is_log=x_is_log, save_dir=save_dir, **kwargs)
+    plot_predicted_variances_box(variable_name, variable_values, predicted_variance, predicted_residual,
+                                 scalars, x_is_log=x_is_log, save_dir=save_dir, **kwargs)
     # plot_prediction_error(variable_values, variable_name, predicted_variance, predicted_residual,
     #                       scalars, x_is_log=x_is_log, **kwargs)
     plot_prediction_scatter(variable_name, variable_values, predicted_variance, predicted_residual,
