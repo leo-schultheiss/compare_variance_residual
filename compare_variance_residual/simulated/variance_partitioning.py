452359f932aeeb83b72bcc002989a07b6ad38df3
Leo Schultheiß <leo.schultheiss@tum.de>
2025-01-27 16:59:29 +0100

    add all possible scores to variance_partitioning.py

--- compare_variance_residual/simulated/variance_partitioning.py
+++ compare_variance_residual/simulated/variance_partitioning.py
@@ -4,7 +4,7 @@ from himalaya.ridge import RidgeCV, GroupRidgeCV
 
 
 def variance_partitioning(Xs_train, Xs_test, Y_train, Y_test, alphas=np.logspace(-10, 10, 41), cv=10,
-                          direct_vp=False, use_r2=True):
+                          use_r2=True) -> tuple:
     """
     Perform variance partitioning on two feature spaces
 
@@ -22,48 +22,46 @@ def variance_partitioning(Xs_train, Xs_test, Y_train, Y_test, alphas=np.logspace
         List of alphas for Ridge regression
     cv : int, default=10
         Number of cross-validation folds
-    direct_vp : bool, default=False
-        Whether to use direct result from feature space 1 (joint model - feature space 1)
-        or to use both feature spaces (feature space 0 - shared)
     use_r2 : bool, default=True
         Determines the score metric: if True, use r2 as the score metric; if False, use correlation.
 
     Returns
     ----------
-    returns : float
-        proportional unique variance explained by feature space 0
+    score_0 : float
+        Total variance explained by the first feature space.
+    score_1 : float
+        Total variance explained by the second feature space.
+    joint_score : float
+        Total variance explained by both feature spaces.
+    x0_unique : float
+        Unique variance explained by the first feature space.
+    x1_unique : float
+        Unique variance explained by the second feature space.
+    shared : float
+        Shared variance between the two feature spaces.
     """
 
     score_func = himalaya.scoring.r2_score if use_r2 else himalaya.scoring.correlation_score
 
-    # train joint model
     solver_params = dict(n_iter=10, alphas=alphas, progress_bar=False, warn=False, score_func=score_func)
-    model = GroupRidgeCV(groups="input", solver_params=solver_params)
-    model.fit(Xs_train, Y_train)
-    joint_score = model.score(Xs_test, Y_test)
+    # train joint model
+    joint_model = GroupRidgeCV(groups="input", solver_params=solver_params)
+    joint_model.fit(Xs_train, Y_train)
+    joint_score = joint_model.score(Xs_test, Y_test)
 
-    # train single model(s)
     solver_params = dict(warn=False, score_func=score_func)
-    if direct_vp:
-        model = RidgeCV(alphas=alphas, cv=cv, solver_params=solver_params)
-        model.fit(Xs_train[1], Y_train)
-        score = model.score(Xs_test[1], Y_test)
+    # train single model(s)
+    model_0 = RidgeCV(alphas=alphas, cv=cv, solver_params=solver_params)
+    model_0.fit(Xs_train[0], Y_train)
+    score_0 = model_0.score(Xs_test[0], Y_test)
 
-        # calculate unique variance explained by feature space 0 only using the joint model and feature space 1
-        X0_unique = joint_score - score
-    else:
-        # train both models
-        model_0 = RidgeCV(alphas=alphas, cv=cv, solver_params=solver_params)
-        model_0.fit(Xs_train[0], Y_train)
-        score_0 = model_0.score(Xs_test[0], Y_test)
+    model_1 = RidgeCV(alphas=alphas, cv=cv, solver_params=solver_params)
+    model_1.fit(Xs_train[1], Y_train)
+    score_1 = model_1.score(Xs_test[1], Y_test)
 
-        model_1 = RidgeCV(alphas=alphas, cv=cv, solver_params=solver_params)
-        model_1.fit(Xs_train[1], Y_train)
-        score_1 = model_1.score(Xs_test[1], Y_test)
+    # calculate unique variance explained by feature space 0
+    shared = (score_0 + score_1) - joint_score
+    x0_unique = score_0 - shared
+    x1_unique = score_1 - shared
 
-        # calculate unique variance explained by feature space 0
-        shared = (score_0 + score_1) - joint_score
-        X0_unique = score_0 - shared
-
-    mean = np.mean(X0_unique)
-    return float(mean)
+    return score_0, score_1, joint_score, x0_unique, x1_unique, shared
