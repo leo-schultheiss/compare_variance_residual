3b12fac55862e66e8f50aa8d6e352ab8d501504b
Leo <leo.schultheiss@tum.de>
2025-02-01 21:02:54 +0100

    fix orthogonal feature spaces

--- compare_variance_residual/tests/test_simulation.py
+++ compare_variance_residual/tests/test_simulation.py
@@ -8,32 +8,27 @@ from compare_variance_residual.simulated.simulation import create_orthogonal_fea
 
 class TestSimulationFunctions(unittest.TestCase):
     feature_spaces = None
-
-    def tearDown(self):
-        self.feature_spaces = None
+    num_samples = None
+    d_list = None
+    atol = 1e-10
 
     def setUp(self):
         # Test the function
-        num_samples = random.randint(1, 100)
-        d_list = [random.randint(1, 100) for _ in range(3)]
-        self.feature_spaces = create_orthogonal_feature_spaces(num_samples, d_list)
-
-    def test_orthogonality_within_feature_space(self):
-        for i, space in enumerate(self.feature_spaces):
-            # Verify orthogonality within each space
-            assert np.allclose(space.T @ space, np.eye(space.shape[1]))
+        self.d_list = [random.randint(1, 100) for _ in range(3)]
+        self.num_samples = random.randint(sum(self.d_list) + 1, sum(self.d_list) + 100)
+        self.feature_spaces = create_orthogonal_feature_spaces(self.num_samples, self.d_list)
 
     def test_orthogonality_between_feature_spaces(self):
         # Verify orthogonality between feature spaces
         for i in range(len(self.feature_spaces) - 1):
             for j in range(i + 1, len(self.feature_spaces)):
                 product = self.feature_spaces[i].T @ self.feature_spaces[j]
-                assert np.allclose(product, np.zeros_like(self.feature_spaces[i].T @ self.feature_spaces[j]), atol=1e-2)
+                assert np.allclose(product, np.zeros_like(self.feature_spaces[i].T @ self.feature_spaces[j]), atol=self.atol)
 
     def test_feature_means(self):
         # make sure means are close to 0
         for space in self.feature_spaces:
-            assert np.allclose(space.mean(axis=0), 0, atol=1e-2)
+            assert np.allclose(space.mean(axis=0), 0, atol=self.atol)
 
 
 if __name__ == '__main__':
