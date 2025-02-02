a1a52063305a4e1bd3a99b2af39c4cbbf488172e
Leo <leo.schultheiss@tum.de>
2025-02-02 11:42:27 +0100

    Refactor test method placement for clarity

--- compare_variance_residual/tests/test_simulation.py
+++ compare_variance_residual/tests/test_simulation.py
@@ -18,17 +18,17 @@ class TestSimulationFunctions(unittest.TestCase):
         self.num_samples = random.randint(sum(self.d_list) + 1, sum(self.d_list) + 100)
         self.feature_spaces = create_orthogonal_feature_spaces(self.num_samples, self.d_list)
 
+    def test_feature_means(self):
+        # make sure means are close to 0
+        for space in self.feature_spaces:
+            assert np.allclose(space.mean(axis=0), 0, atol=self.atol)
+
     def test_orthogonality_between_feature_spaces(self):
         # Verify orthogonality between feature spaces
         for i in range(len(self.feature_spaces) - 1):
             for j in range(i + 1, len(self.feature_spaces)):
                 product = self.feature_spaces[i].T @ self.feature_spaces[j]
                 assert np.allclose(product, np.zeros_like(self.feature_spaces[i].T @ self.feature_spaces[j]), atol=self.atol)
-
-    def test_feature_means(self):
-        # make sure means are close to 0
-        for space in self.feature_spaces:
-            assert np.allclose(space.mean(axis=0), 0, atol=self.atol)
 
 
 if __name__ == '__main__':
