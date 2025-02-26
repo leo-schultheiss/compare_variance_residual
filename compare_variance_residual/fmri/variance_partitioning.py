09affcf37b28550daafac916345ee2fdded162d5
Leo <leo.schultheiss@tum.de>
2025-02-26 11:31:43 +0100

    init variance_partitioning.py

---
+++ compare_variance_residual/fmri/variance_partitioning.py
@@ -0,0 +1,78 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+from fmri.features import load_feature, load_brain_data
+from fmri.results import get_result_path
+from fmri.ridge import run_ridge_pipeline, run_banded_pipeline
+
+
+def signed_square(r):
+    return r ** 2 * np.sign(r)
+
+
+def variance_partitioning(data_dir, subject, modality, low_level_feature, alphas, cv, number_of_delays, n_targets_batch,
+                          n_alphas_batch,
+                          n_targets_batch_refit, n_iter):
+    path = get_result_path(modality, subject)
+
+    print("Loading data")
+    Y, n_samples_train = load_brain_data(data_dir, subject, modality)
+    X_semantic, n_samples_train = load_feature(data_dir, "english1000")
+    X_low_level, n_samples_train = load_feature(data_dir, low_level_feature)
+    print("Done loading data")
+
+    # Variance Partitioning
+    print("Running Variance Partitioning")
+    low_level_path = os.path.join(path, f"{low_level_feature}_scores.csv")
+    if not os.path.exists(low_level_path):
+        print("Running low level")
+        low_level_scores = run_ridge_pipeline(X_low_level, Y, n_samples_train, alphas, cv,
+                                              number_of_delays, n_targets_batch, n_alphas_batch,
+                                              n_targets_batch_refit)
+        low_level_scores.to_csv(low_level_path, index=False)
+    else:
+        print("Loading low level")
+        low_level_scores = pd.read_csv(low_level_path)
+
+    english1000_path = os.path.join(path, f"english1000_scores.csv")
+    if not os.path.exists(english1000_path):
+        print("Running english1000")
+        english1000_scores = run_ridge_pipeline(X_semantic, Y, n_samples_train, alphas, cv, number_of_delays,
+                                                n_targets_batch,
+                                                n_alphas_batch, n_targets_batch_refit)
+        english1000_scores.to_csv(english1000_path, index=False)
+    else:
+        print("Loading english1000")
+        english1000_scores = pd.read_csv(english1000_path)
+
+    joint_path = os.path.join(path, f"joint_english1000_{low_level_feature}_scores.csv")
+    if not os.path.exists(joint_path):
+        joint_features = np.concatenate([X_semantic, X_low_level], axis=1)
+        n_features_list = [X_semantic.shape[1], X_low_level.shape[1]]
+        joint_scores = run_banded_pipeline(joint_features, n_features_list, Y, n_samples_train, alphas, cv, n_iter,
+                                           number_of_delays, n_targets_batch, n_alphas_batch, n_targets_batch_refit)
+        joint_scores.to_csv(joint_path, index=False)
+    else:
+        joint_scores = pd.read_csv(joint_path)
+
+    vp_path = os.path.join(path, f"vp_english1000_{low_level_feature}_scores.csv")
+    if not os.path.exists(vp_path):
+        # perform vp
+        vp_english1000 = pd.DataFrame()
+        # iterate all columns containining "correlation"
+        for corr in [col for col in joint_scores.columns if "correlation" in col]:
+            # get the column name without the suffix
+            col = corr.split("_")
+            # get the intersection of the two sets
+            intersection = signed_square(english1000_scores[col]) + signed_square(
+                low_level_scores[col]) - signed_square(joint_scores[col])
+            difference = signed_square(english1000_scores[col]) - signed_square(intersection)
+
+            vp_english1000[fr'semantic$\cup${low_level_feature}'] = intersection
+            vp_english1000[f'semantic\\{low_level_feature}'] = difference
+        vp_english1000.to_csv(vp_path, index=False)
+    else:
+        vp_english1000 = pd.read_csv(vp_path)
+    return vp_english1000
