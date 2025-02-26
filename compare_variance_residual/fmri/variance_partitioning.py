232d2be636185cefaeb9971fdabb86d33af3fbfc
Leo <leo.schultheiss@tum.de>
2025-02-26 13:24:03 +0100

    set default parameters for residual and vp

--- compare_variance_residual/fmri/variance_partitioning.py
+++ compare_variance_residual/fmri/variance_partitioning.py
@@ -12,8 +12,9 @@ def signed_square(r):
     return r ** 2 * np.sign(r)
 
 
-def variance_partitioning(data_dir, subject, modality, low_level_feature, alphas, cv, number_of_delays, n_targets_batch,
-                          n_alphas_batch, n_targets_batch_refit, n_iter, X_semantic=None, X_low_level=None, Y=None, n_samples_train=None):
+def variance_partitioning(data_dir, subject, modality, low_level_feature, alphas=np.logspace(-5, 20, 26), cv=5,
+                          number_of_delays=4, n_targets_batch=100, n_alphas_batch=3, n_targets_batch_refit=50,
+                          n_iter=10, X_semantic=None, X_low_level=None, Y=None, n_samples_train=None):
     path = get_result_path(modality, subject)
 
     print("Loading data")
