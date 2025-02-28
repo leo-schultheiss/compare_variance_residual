4d602c4fc89d0118009a3ac39ebc3efa395546c0
Leo <leo.schultheiss@tum.de>
2025-02-28 11:06:14 +0100

    compute ev and p-values

--- compare_variance_residual/fmri/batch_run_brain_maps.py
+++ compare_variance_residual/fmri/batch_run_brain_maps.py
@@ -1,7 +1,12 @@
+import os
+
+import numpy as np
 from himalaya.backend import set_backend
+from voxelwise_tutorials.viz import plot_flatmap_from_mapper
 
 from fmri.features import load_brain_data, load_feature
 from fmri.residual_method import residual_method
+from fmri.results import get_result_path
 from fmri.variance_partitioning import variance_partitioning
 
 data_dir = "../../data"
@@ -17,7 +22,8 @@ if __name__ == "__main__":
     for subject in range(1, 10):
         for low_level_feature, modality in zip(['letters', 'phonemes'], ["reading", "listening"]):
             print(f"Running {subject} {modality} {low_level_feature}")
-            Y, _, run_onsets = load_brain_data(data_dir, subject, modality)
+            Y, _, run_onsets, ev = load_brain_data(data_dir, subject, modality)
+            np.savetxt(os.path.join(get_result_path(modality, subject), "ev.csv"), ev, delimiter=",", header="ev")
             X_low_level, n_samples_train = load_feature(data_dir, low_level_feature)
             variance_partitioning(data_dir, subject, modality, low_level_feature, run_onsets,
                                   n_targets_batch=n_targets_batch, n_alphas_batch=n_alphas_batch,
