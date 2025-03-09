f50ca0198170fbad2d9635a581e08b4114b8eb59
Leo <leo.schultheiss@tum.de>
2025-03-09 19:21:21 +0100

    run batch predictions for moten and powspec

--- compare_variance_residual/fmri/batch_run_brain_maps.py
+++ compare_variance_residual/fmri/batch_run_brain_maps.py
@@ -11,24 +11,16 @@ from variance_partitioning import variance_partitioning
 data_dir = "../../data"
 backend = set_backend('torch_cuda', on_error='warn')
 
-n_targets_batch = 100
-n_targets_batch_refit = 50
-n_alphas_batch = 5
-
 if __name__ == "__main__":
     X_semantic, n_samples_train = load_feature(data_dir, "english1000")
 
     for subject in range(1, 10):
-        for low_level_feature, modality in zip(['letters', 'phonemes'], ["reading", "listening"]):
+        for low_level_feature, modality in zip(['moten', 'powspec'], ["reading", "listening"]):
             print(f"Running {subject} {modality} {low_level_feature}")
-            Y, _, run_onsets, ev = load_brain_data(data_dir, subject, modality)
+            Y, _, ev = load_brain_data(data_dir, subject, modality)
             np.savetxt(os.path.join(get_result_path(modality, subject), "ev.csv"), ev, delimiter=",", header="ev")
             X_low_level, n_samples_train = load_feature(data_dir, low_level_feature)
-            variance_partitioning(data_dir, subject, modality, low_level_feature, run_onsets,
-                                  n_targets_batch=n_targets_batch, n_alphas_batch=n_alphas_batch,
-                                  n_targets_batch_refit=n_targets_batch_refit, X_semantic=X_semantic,
+            variance_partitioning(data_dir, subject, modality, low_level_feature, X_semantic=X_semantic,
                                   X_low_level=X_low_level, Y=Y, n_samples_train=n_samples_train)
-            residual_method(data_dir, subject, modality, low_level_feature, run_onsets,
-                            n_targets_batch=n_targets_batch, n_alphas_batch=n_alphas_batch,
-                            n_targets_batch_refit=n_targets_batch_refit, X_semantic=X_semantic,
+            residual_method(data_dir, subject, modality, low_level_feature, X_semantic=X_semantic,
                             X_low_level=X_low_level, Y=Y, n_samples_train=n_samples_train)
