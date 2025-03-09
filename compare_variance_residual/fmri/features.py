39731a57757a47344efa4aeb699b3b86b858a657
Leo <leo.schultheiss@tum.de>
2025-03-09 16:14:18 +0100

    trim 10 TRs to align with powspec feature

--- compare_variance_residual/fmri/features.py
+++ compare_variance_residual/fmri/features.py
@@ -8,7 +8,7 @@ from voxelwise_tutorials.io import load_hdf5_array
 from voxelwise_tutorials.utils import explainable_variance
 
 
-def load_brain_data(data_dir, subject, modality, trim=5):
+def load_brain_data(data_dir, subject, modality, trim=10):
     Y_train_filename = os.path.join(data_dir, 'responses', f'subject{subject:02}_{modality}_fmri_data_trn.hdf')
     Y_test_filename = os.path.join(data_dir, 'responses', f'subject{subject:02}_{modality}_fmri_data_val.hdf')
     Y_train_hdf = load_hdf5_array(Y_train_filename)
@@ -25,7 +25,7 @@ def load_brain_data(data_dir, subject, modality, trim=5):
             print("Encapsulated story")
             story_data = story_data[0]
 
-        story_data = story_data[:-trim]
+        story_data = story_data[trim:-trim]
 
         story_data = story_data.astype(np.float32)
         story_data = np.nan_to_num(story_data)
@@ -37,7 +37,6 @@ def load_brain_data(data_dir, subject, modality, trim=5):
             Y_train = np.vstack([Y_train, story_data])
 
         run_onsets.append(run_onsets[-1] + story_data.shape[0])
-    # remove last element
     run_onsets = run_onsets[:-1]
 
     n_samples_train = Y_train.shape[0]
@@ -45,7 +44,7 @@ def load_brain_data(data_dir, subject, modality, trim=5):
     Y_test = []
     eval_story = first(Y_test_hdf.keys())
     for i in range(2):
-        story_data = Y_test_hdf[eval_story][i][trim:-(trim + 5)]
+        story_data = Y_test_hdf[eval_story][i][trim:-trim]
         story_data = story_data.astype(np.float32)
         story_data = np.nan_to_num(story_data)
         story_data = zscore(story_data)
@@ -61,21 +60,19 @@ def load_brain_data(data_dir, subject, modality, trim=5):
     return Y, n_samples_train, run_onsets, ev
 
 
-def load_feature(data_dir, feature_name, trim=5):
+def load_feature(data_dir, feature_name, trim=10):
     if feature_name not in ['powspec', 'moten']:
         Xs_train = h5py.File(os.path.join(data_dir, 'features', 'features_trn_NEW.hdf'), 'r')
         Xs_val = h5py.File(os.path.join(data_dir, 'features', 'features_val_NEW.hdf'), 'r')
-
-        X_train = np.vstack([zscore(Xs_train[story][feature_name][trim:-(trim + 5)]) for story in Xs_train.keys()])
-        n_samples_train = X_train.shape[0]
+        X_train = np.vstack([zscore(Xs_train[story][feature_name][trim:-trim]) for story in Xs_train.keys()])
         X_val = np.vstack([zscore(Xs_val[story][feature_name]) for story in Xs_val.keys()])
     elif feature_name == 'powspec':
         X_train = h5py.File(os.path.join(data_dir, 'features', 'features_matrix.hdf'), 'r')['powspec_train']
-        n_samples_train = X_train.shape[0]
         X_val = h5py.File(os.path.join(data_dir, 'features', 'features_matrix.hdf'), 'r')['powspec_test']
     else:
         raise(ValueError(f"Feature {feature_name} not found/implemented"))
 
+    n_samples_train = X_train.shape[0]
     X = np.vstack([X_train, X_val])
     X = X.astype(np.float32)
     X = np.nan_to_num(X)
